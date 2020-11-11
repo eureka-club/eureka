e61a91d7ce7dd912ba8c12d35cb48c6fd86fc7e9
srigi <srigi@srigi.sk>
2020-11-11 09:34:30 +0100

    Refactor custom server
    - use Typescript
    - simplify code after app.prepare()

---
+++ server.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import { existsSync, mkdirSync } from 'fs';
+import next from 'next';
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const port = process.env.PORT != null ? parseInt(process.env.PORT, 10) : 3000;
+const { NEXT_PUBLIC_LOCAL_ASSETS_BASE_URL, LOCAL_ASSETS_HOST_DIR } = process.env;
+
+app.prepare().then(() => {
+  const server = express();
+
+  if (LOCAL_ASSETS_HOST_DIR != null && !existsSync(LOCAL_ASSETS_HOST_DIR)) {
+    mkdirSync(LOCAL_ASSETS_HOST_DIR);
+  }
+
+  if (LOCAL_ASSETS_HOST_DIR != null && NEXT_PUBLIC_LOCAL_ASSETS_BASE_URL != null) {
+    server.use(
+      NEXT_PUBLIC_LOCAL_ASSETS_BASE_URL,
+      express.static(LOCAL_ASSETS_HOST_DIR, {
+        etag: false,
+        maxAge: dev ? '1h' : '30d',
+      }),
+    );
+  }
+
+  server.use((req, res) => handle(req, res));
+
+  server.listen(port, () => {
+    console.info(`> Ready on http://localhost:${port}`); // eslint-disable-line no-console
+  });
+});
