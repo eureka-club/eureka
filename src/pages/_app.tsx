20223e5dc7dc9c76d77488d576ce8c69d81f1151
srigi <srigi@srigi.sk>
2020-11-16 09:42:16 +0100

    Query FullPostDetail when modal displays Post
    Also refactor react-bootstrap imports to import individual components.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import { AppProps } from 'next/app';
 import { StrictMode, FunctionComponent } from 'react';
+import { QueryCache, ReactQueryCacheProvider } from 'react-query';
 
 import './_app.css';
 
+const queryCache = new QueryCache();
+
 const App: FunctionComponent<AppProps> = ({ Component, pageProps }) => (
   <StrictMode>
-    {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-    <Component {...pageProps} />
+    <ReactQueryCacheProvider queryCache={queryCache}>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </ReactQueryCacheProvider>
   </StrictMode>
 );
 
