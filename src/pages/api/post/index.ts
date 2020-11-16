6e2c4bb9c1556136ad8a68ea07968af24d0018a0
srigi <srigi@srigi.sk>
2020-11-16 19:00:27 +0100

    Store Post (+LocalImage, +Work) to Db

--- src/pages/api/post/index.ts
+++ src/pages/api/post/index.ts
@@ -1,7 +1,38 @@
+import crypto from 'crypto';
+import fs, { existsSync, mkdirSync } from 'fs';
+import { Form } from 'multiparty';
 import { NextApiRequest, NextApiResponse } from 'next';
-import multiparty from 'multiparty';
+import path from 'path';
+import * as uuid from 'uuid';
 
 import getApiHandler from '../../../lib/getApiHandler';
+import { getDbConnection } from '../../../lib/db';
+
+type FileUpload = {
+  fieldName: string;
+  originalFilename: string;
+  path: string;
+  headers: Record<string, string>;
+  size: number;
+};
+
+type PostProps = {
+  workTitle: string[];
+  workAuthor: string[];
+  workLink: string[];
+  workType: string[];
+  language: string[];
+  hashtags: string[];
+  description: string[];
+  public: string[];
+};
+
+const { LOCAL_ASSETS_HOST_DIR } = process.env;
+const DB_TABLE_LOCAL_IMAGE = 'local_image';
+const DB_TABLE_POST = 'post';
+const DB_TABLE_WORK = 'work';
+const FOLDER_SPREAD_CHAR_COUNT = 2;
+const TEMPORARY_POST_CREATOR_UUID = '340c3ef1-d00b-4480-b9a3-60ce197fdff8';
 
 export const config = {
   api: {
@@ -9,14 +40,114 @@ export const config = {
   },
 };
 
+const getFileHash = (filePath: string, algorithm = 'sha1'): string => {
+  const fileContent = fs.readFileSync(filePath);
+  const sha1sum = crypto.createHash(algorithm);
+  sha1sum.update(fileContent);
+
+  return sha1sum.digest('hex');
+};
+
+const moveUploadedFileByHash = (fileUpload: FileUpload, fileHash: string): string => {
+  const fileDestDir = path.join(LOCAL_ASSETS_HOST_DIR!, fileHash.substr(0, FOLDER_SPREAD_CHAR_COUNT));
+  if (!existsSync(fileDestDir)) {
+    mkdirSync(fileDestDir);
+  }
+
+  const fileDestPath = path.join(fileDestDir, fileHash + path.extname(fileUpload.path));
+  console.info('Storing file upload under...', fileDestPath); // eslint-disable-line no-console
+  fs.renameSync(fileUpload.path, fileDestPath);
+
+  return fileDestPath;
+};
+
+const saveImageUploadToDB = async (
+  imageUpload: FileUpload,
+  imageDestPath: string,
+  imageHash: string,
+): Promise<string> => {
+  const assetsPathPrefixClean = LOCAL_ASSETS_HOST_DIR!.replace('./', '');
+  const cutLength = (assetsPathPrefixClean + path.delimiter).length;
+
+  const connection = await getDbConnection();
+  const table = connection(DB_TABLE_LOCAL_IMAGE);
+  const pk = uuid.v4();
+  await table.insert({
+    id: pk,
+    original_filename: imageUpload.originalFilename,
+    stored_file: imageDestPath.substr(cutLength),
+    mime_type: imageUpload.headers['content-type'],
+    content_hash: imageHash,
+  });
+
+  return pk;
+};
+
+const getPostWork = async (postProps: PostProps): Promise<string> => {
+  const connection = await getDbConnection();
+  const table = connection(DB_TABLE_WORK);
+
+  const existingWork = await table
+    .where({
+      author: postProps.workAuthor[0],
+      title: postProps.workTitle[0],
+      type: postProps.workType[0],
+    })
+    .first();
+  if (existingWork != null) {
+    return existingWork.id;
+  }
+
+  const pk = uuid.v4();
+  await table.insert({
+    id: pk,
+    type: postProps.workType[0],
+    title: postProps.workTitle[0],
+    author: postProps.workAuthor[0],
+    year_created: '1900',
+    link: postProps.workLink[0],
+  });
+
+  return pk;
+};
+
+const savePost = async (postProps: PostProps, localImageUuid: string, workUuid: string): Promise<string> => {
+  const connection = await getDbConnection();
+  const table = connection(DB_TABLE_POST);
+
+  const pk = uuid.v4();
+  await table.insert({
+    id: pk,
+    creator_id: TEMPORARY_POST_CREATOR_UUID,
+    local_image_id: localImageUuid,
+    work_id: workUuid,
+    content_text: postProps.description[0],
+  });
+
+  return pk;
+};
+
 export default getApiHandler().post<NextApiRequest, NextApiResponse>(
   async (req, res): Promise<void> => {
-    const form = new multiparty.Form();
+    new Form().parse(req, async (err, fields, files) => {
+      if (err != null) {
+        console.error(err); // eslint-disable-line no-console
+        res.status(500).end();
+        return;
+      }
+      if (files?.image == null) {
+        res.status(422).json({ error: 'no image received' });
+        return;
+      }
 
-    form.parse(req, (err, fields, files) => {
-      console.log('>>>', { err, fields, image: files.image });
+      const imageUpload: FileUpload = files.image[0];
+      const imageHash = getFileHash(imageUpload.path);
+      const imageDestPath = moveUploadedFileByHash(imageUpload, imageHash);
+      const localImageDbRecordUuid = await saveImageUploadToDB(imageUpload, imageDestPath, imageHash);
+      const workDbRecordUuid = await getPostWork(fields);
+      const postUuid = await savePost(fields, localImageDbRecordUuid, workDbRecordUuid);
 
-      res.json({ status: 'OK!' });
+      res.json({ postUuid });
     });
   },
 );
