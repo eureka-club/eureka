227c4532d69cef62118042424cd9817e0ed38012
srigi <srigi@srigi.sk>
2021-02-01 17:53:57 +0100

    Create a new Work via API call

---
+++ src/facades/work.ts
@@ -0,0 +1,42 @@
+import { Work } from '@prisma/client';
+
+import { CreateWorkServerFields, CreateWorkServerPayload, StoredFileUpload } from '../types';
+import prisma from '../lib/prisma';
+
+export const createWork = async (fields: CreateWorkServerFields, coverImageUpload: StoredFileUpload): Promise<Work> => {
+  const payload = Object.entries(fields).reduce((memo, field) => {
+    const [fieldName, fieldValues] = field;
+
+    if (fieldName === 'publicationYear') {
+      return { ...memo, [fieldName]: new Date(fieldValues[0]) };
+    }
+
+    return { ...memo, [fieldName]: fieldValues[0] };
+  }, {} as CreateWorkServerPayload);
+
+  const existingLocalImage = await prisma.localImage.findFirst({
+    where: { contentHash: coverImageUpload.contentHash },
+  });
+
+  if (existingLocalImage != null) {
+    return prisma.work.create({
+      data: {
+        ...payload,
+        localImages: {
+          connect: {
+            id: existingLocalImage.id,
+          },
+        },
+      },
+    });
+  }
+
+  return prisma.work.create({
+    data: {
+      ...payload,
+      localImages: {
+        create: { ...coverImageUpload },
+      },
+    },
+  });
+};
