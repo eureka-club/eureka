efac644bc621056e7b4934c95d4484a0c3d9590a
srigi <srigi@srigi.sk>
2021-02-01 18:58:10 +0100

    Delete <Mosaic> from IndexPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,18 @@
-import dayjs from 'dayjs';
 import { GetServerSideProps, NextPage } from 'next';
 
-import { DATE_FORMAT_PROPS } from '../constants';
 import SimpleLayout from '../components/layouts/SimpleLayout';
-import Mosaic from '../components/Mosaic';
-import { PostDetail } from '../types';
-import { fetchIndexMosaic } from '../repositories/Post';
 
-interface Props {
-  posts: PostDetail[];
-}
-
-const IndexPage: NextPage<Props> = ({ posts }) => {
+const IndexPage: NextPage = () => {
   return (
     <SimpleLayout title="Welcome">
-      <Mosaic stack={posts} />
+      <h1>Hello</h1>
     </SimpleLayout>
   );
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const posts = await fetchIndexMosaic();
-  const serializablePosts = posts.map((post) => {
-    return {
-      ...post,
-      ...(post['cycle.start_date'] != null && {
-        'cycle.start_date': dayjs(post['cycle.start_date']).format(DATE_FORMAT_PROPS),
-      }),
-      ...(post['cycle.end_date'] != null && {
-        'cycle.end_date': dayjs(post['cycle.start_date']).format(DATE_FORMAT_PROPS),
-      }),
-    };
-  });
-
   return {
-    props: {
-      posts: serializablePosts,
-    },
+    props: {},
   };
 };
 
