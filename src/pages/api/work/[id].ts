9e39817319fc5c4d8cd46c52f60ae25994d49dd7
srigi <srigi@srigi.sk>
2021-02-03 22:31:19 +0100

    Add API endpoint to delete Work

---
+++ src/pages/api/work/[id].ts
@@ -0,0 +1,46 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/client';
+
+import { Session } from '../../../types';
+import getApiHandler from '../../../lib/getApiHandler';
+import { findOne, remove } from '../../../facades/work';
+import prisma from '../../../lib/prisma';
+
+export default getApiHandler().delete<NextApiRequest, NextApiResponse>(
+  async (req, res): Promise<void> => {
+    const session = (await getSession({ req })) as Session;
+    if (session == null) {
+      res.status(401).json({ status: 'Unauthorized' });
+      return;
+    }
+
+    const { id } = req.query;
+    if (typeof id !== 'string') {
+      res.status(404).end();
+      return;
+    }
+
+    const idNum = parseInt(id, 10);
+    if (!Number.isInteger(idNum)) {
+      res.status(404).end();
+      return;
+    }
+
+    try {
+      const work = await findOne(idNum);
+      if (work == null) {
+        res.status(404).end();
+        return;
+      }
+
+      await remove(idNum);
+
+      res.status(200).json({ status: 'OK' });
+    } catch (exc) {
+      console.error(exc); // eslint-disable-line no-console
+      res.status(500).json({ status: 'server error' });
+    } finally {
+      prisma.$disconnect();
+    }
+  },
+);
