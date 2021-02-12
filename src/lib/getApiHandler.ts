76ba106f213d62c06bcbabc0a8398013362fe47c
srigi <srigi@srigi.sk>
2021-02-12 16:26:46 +0100

    Add general API error handler

--- src/lib/getApiHandler.ts
+++ src/lib/getApiHandler.ts
@@ -4,6 +4,18 @@ import nc from 'next-connect';
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default () =>
   nc<NextApiRequest, NextApiResponse>({
+    onError(err, req, res) {
+      const matches = err.message.match(/^\[(\d+)]\s(.*)/);
+      if (matches != null) {
+        const [, status, message] = matches;
+        res.status(status).json({ error: message });
+        return;
+      }
+
+      console.error(err); // eslint-disable-line no-console
+      res.status(500).json({ error: 'Server error' });
+    },
+
     onNoMatch(req, res) {
       res.status(405).end();
     },
