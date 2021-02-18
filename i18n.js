0a156403ddde2f958e02029832745900911a2d68
srigi <srigi@srigi.sk>
2021-02-18 15:10:29 +0100

    Change default locale to ES
     - also fix page(s) setup for i18n for Post detail to load Cycle/Work detail
    namespace

--- i18n.js
+++ i18n.js
@@ -1,12 +1,12 @@
 module.exports = {
   loader: false, // because we use babel plugins 😕
   loadLocaleFrom: async (locale, namespace) => import(`./translations/${locale}/${namespace}`).then((m) => m.default),
-  locales: ['en', 'es', 'fr', 'pt'],
-  defaultLocale: 'en',
+  locales: ['es', 'en', 'fr', 'pt'],
+  defaultLocale: 'es',
   pages: {
     '*': ['common', 'createWorkForm', 'createPostForm', 'navbar', 'signInForm'],
     '/cycle/create': ['createCycleForm'],
-    '/cycle/[id]': ['cycleDetail'],
-    '/work/[id]': ['workDetail'],
+    'rgx:^/cycle/\\[id\\]': ['cycleDetail'],
+    'rgx:^/work/\\[id\\]': ['workDetail'],
   },
 };
