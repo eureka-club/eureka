af7e592d448959d072c029351de0bdf88b206340
srigi <srigi@srigi.sk>
2021-02-19 14:38:57 +0100

    Configure next-auth email provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,13 @@
 import { User } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
-import NextAuth from 'next-auth';
+import NextAuth, { InitOptions } from 'next-auth';
 import Adapters from 'next-auth/adapters';
 import Providers from 'next-auth/providers';
 
 import prisma from '../../../src/lib/prisma';
 import { Session } from '../../../src/types';
 
-const options = {
+const options: InitOptions = {
   adapter: Adapters.Prisma.Adapter({ prisma }),
   callbacks: {
     session: async (session: Session, user: User) => {
@@ -17,10 +17,22 @@ const options = {
       return Promise.resolve(session);
     },
   },
+  debug: process.env.NODE_ENV === 'development',
   providers: [
     Providers.Google({
       clientId: process.env.GOOGLE_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+    Providers.Email({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST!,
+        port: Number(process.env.EMAIL_SERVER_PORT!),
+        auth: {
+          user: process.env.EMAIL_SERVER_USER!,
+          pass: process.env.EMAIL_SERVER_PASS!,
+        },
+      },
+      from: process.env.EMAILING_FROM,
     }),
   ],
   secret: process.env.SECRET,
