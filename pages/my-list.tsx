8aaa05d463ca3f00d9edbe7631fafbdc8e7c5d6a
rickrebel <rickrebel@gmail.com>
2021-03-07 14:07:57 -0600

    Now favs Cycles and favs Books are visibles from 'MyList

--- pages/my-list.tsx
+++ pages/my-list.tsx
@@ -1,27 +1,23 @@
 import { GetServerSideProps, NextPage } from 'next';
 import { getSession } from 'next-auth/client';
 import useTranslation from 'next-translate/useTranslation';
+import { flatten, zip } from 'lodash';
 
 import { Session } from '../src/types';
 import { CycleMosaicItem } from '../src/types/cycle';
+import { WorkMosaicItem } from '../src/types/work';
+import { PostMosaicItem } from '../src/types/post';
 import SimpleLayout from '../src/components/layouts/SimpleLayout';
 import { search as searchCycles } from '../src/facades/cycle';
+import { search as searchPosts } from '../src/facades/post';
+import { search as searchWork } from '../src/facades/work';
 import Mosaic from '../src/components/Mosaic';
+import { MosaicItem } from '../src/types';
+
 
 interface Props {
-  cycles: CycleMosaicItem[];
+  myListMosaicData: (CycleMosaicItem | WorkMosaicItem)[];
 }
-
-const MyListPage: NextPage<Props> = ({ cycles }) => {
-  const { t } = useTranslation('common');
-
-  return (
-    <SimpleLayout title={t('browserTitleMyList')}>
-      <h4 className="mt-4 mb-5">{t('mosaicHeaderMyCycles')}</h4>
-      <Mosaic stack={cycles} />
-    </SimpleLayout>
-  );
-};
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const session = (await getSession(ctx)) as Session;
@@ -30,15 +26,47 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   }
 
   const cycles = await searchCycles({
-    where: JSON.stringify({ participants: { some: { id: session.user.id } } }),
+    where: JSON.stringify(
+      {
+        OR: [
+          { participants: { some: { id: session.user.id } } },
+          { favs: { some: { id: session.user.id } } },
+        ]
+      }
+    ),
     include: JSON.stringify({ localImages: true }),
   });
 
+  const posts = await searchPosts({
+    where: JSON.stringify({ favs: { some: { id: session.user.id } } }),
+    include: JSON.stringify({ localImages: true }),
+  });
+
+  const works = await searchWork({
+    where: JSON.stringify({ favs: { some: { id: session.user.id } } }),
+    include: JSON.stringify({ localImages: true }),
+  });
+
+  const interleavedResults = flatten(zip(cycles, works)).filter((workOrCycle) => workOrCycle != null);
+
   return {
     props: {
-      cycles,
+      myListMosaicData: interleavedResults,      
     },
   };
 };
+
+
+const MyListPage: NextPage<Props> = ({ myListMosaicData }) => {
+  const { t } = useTranslation('common');
+
+  return (
+    <SimpleLayout title={t('browserTitleMyList')}>
+      <h4 className="mt-4 mb-5">{t('mosaicHeaderMyCycles')}</h4>
+      <Mosaic stack={myListMosaicData} />
+    </SimpleLayout>
+  );
+};
+
 
 export default MyListPage;
