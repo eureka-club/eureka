6285e075f86e4195c3383b1585c2cc651fbf59fc
srigi <srigi@srigi.sk>
2021-05-06 16:17:53 +0200

    Setup Blob storage for public assets
    - also fix formatting of logs
    - also rename bash funcitions & ENVs
    - also setup ENVs example values according to new region

--- .azure/00_infrastructure.sh
+++ .azure/00_infrastructure.sh
@@ -11,7 +11,7 @@ function create_resource_group {
 	az group show --name $1 --output none 2>/dev/null
 	if [[ $? != 0 ]]; then
 		echo -e "${COLOR_RED}Missing!${NC}"
-		printf "Creating ${COLOR_YELLOW}resource group${NC} ${COLOR_BLUE}$1${NC}... "
+		printf "Creating ${COLOR_YELLOW}resource group${NC} ${COLOR_BLUE}$1${NC}...\n"
 		az group create \
 			--location ${REGION} \
 			--name $1 \
@@ -20,12 +20,12 @@ function create_resource_group {
 	echo -e "${COLOR_GREEN}OK${NC}"
 }
 
-function created_database_server {
-	printf "Checking existence of ${COLOR_YELLOW}SQL database server${NC} ${COLOR_BLUE}$2${NC} in resource group ${COLOR_BLUE}$1${NC}... "
+function created_sql_server {
+	printf "Checking existence of ${COLOR_YELLOW}SQL server${NC} ${COLOR_BLUE}$2${NC} in resource group ${COLOR_BLUE}$1${NC}... "
 	az sql server show --resource-group $1 --name $2 --output none 2>/dev/null
 	if [[ $? != 0 ]]; then
 		echo -e "${COLOR_RED}Missing!${NC}"
-		printf "Creating ${COLOR_YELLOW}SQL database server${NC} ${COLOR_BLUE}$2${NC}... "
+		printf "Creating ${COLOR_YELLOW}SQL server${NC} ${COLOR_BLUE}$2${NC}...\n"
 		databaseServerFqdn=$(az sql server create \
 			--resource-group $1 \
 			--name $2 \
@@ -35,7 +35,7 @@ function created_database_server {
 			--output tsv)
 		echo "done"
 
-		printf "Creating ${COLOR_YELLOW}SQL server firewall rule${NC} ${COLOR_BLUE}default${NC} (allows access from Azure services)... "
+		printf "Creating ${COLOR_YELLOW}SQL server firewall rule${NC} ${COLOR_BLUE}default${NC} (allows access from Azure services)...\n"
 		az sql server firewall-rule create \
 			--resource-group $1 \
 			--server $2 \
@@ -45,7 +45,7 @@ function created_database_server {
 			--output none
 		echo "done"
 
-		printf "Creating ${COLOR_YELLOW}SQL server firewall rule${NC} ${COLOR_BLUE}$5${NC}... "
+		printf "Creating ${COLOR_YELLOW}SQL server firewall rule${NC} ${COLOR_BLUE}$5${NC}...\n"
 		az sql server firewall-rule create \
 			--resource-group $1 \
 			--server $2 \
@@ -69,7 +69,7 @@ function created_database {
 	az sql db show --resource-group $1 --server $2 --name $3 --output none 2>/dev/null
 	if [[ $? != 0 ]]; then
 		echo -e "${COLOR_RED}Missing!${NC}"
-		printf "Creating ${COLOR_YELLOW}SQL database${NC} ${COLOR_BLUE}$3${NC}... "
+		printf "Creating ${COLOR_YELLOW}SQL database${NC} ${COLOR_BLUE}$3${NC}...\n"
 		az sql db create \
 			--resource-group $1 \
 			--server $2 \
@@ -85,7 +85,7 @@ function create_app_service_plan {
 	res=$(az appservice plan show --resource-group $1 --name $2)
 	if [[ -z ${res} ]]; then
 		echo -e "${COLOR_RED}Missing!${NC}"
-		printf "Creating ${COLOR_YELLOW}app service plan${NC} ${COLOR_BLUE}$2${NC} in resource group ${COLOR_BLUE}$1${NC}... "
+		printf "Creating ${COLOR_YELLOW}app service plan${NC} ${COLOR_BLUE}$2${NC} in resource group ${COLOR_BLUE}$1${NC}...\n"
 		az appservice plan create \
 			--resource-group $1 \
 			--name $2 \
@@ -96,14 +96,44 @@ function create_app_service_plan {
 	echo -e "${COLOR_GREEN}OK${NC}"
 }
 
+function create_blob_storage {
+	printf "Checking existence of ${COLOR_YELLOW}storage account${NC} ${COLOR_BLUE}$2${NC} in resource group ${COLOR_BLUE}$1${NC}... "
+	az storage account show --resource-group $1 --name $2 --output none 2> /dev/null
+	if [[ $? != 0 ]]; then
+		echo -e "${COLOR_RED}Missing!${NC}"
+		printf "Creating ${COLOR_YELLOW}storage account${NC} ${COLOR_BLUE}$2${NC} in resource group ${COLOR_BLUE}$1${NC}...\n"
+		az storage account create \
+			--resource-group $1 \
+			--name $2 \
+			--access-tier Hot \
+			--min-tls-version TLS1_1 \
+			--sku $3 \
+			--output none
+		connectionString=$(az storage account show-connection-string \
+			--resource-group $1 \
+			--name $2 \
+			--query connectionString \
+			-o tsv)
+
+		az storage container create \
+			--resource-group $1 \
+			--name $4 \
+			--connection-string "$connectionString" \
+			--public-access blob \
+			--output none
+	fi
+	echo -e "${COLOR_GREEN}OK${NC}"
+}
+
 create_resource_group ${RESOURCE_GROUP_APP}
-created_database_server ${RESOURCE_GROUP_APP} ${DATABASE_SERVER_NAME} \
+created_sql_server ${RESOURCE_GROUP_APP} ${DATABASE_SERVER_NAME} \
 	${DATABASE_SERVER_ADMIN_USER} \
 	${DATABASE_SERVER_ADMIN_PASSWORD} \
 	${DATABASE_SERVER_FIREWALL_RULE_NAME} \
-	${DATABASE_SERVER_FIREWALL_START_IP_ASSRESS} \
-	${DATABASE_SERVER_FIREWALL_END_IP_ASSRESS}
+	${DATABASE_SERVER_FIREWALL_START_IP_ADDRESS} \
+	${DATABASE_SERVER_FIREWALL_END_IP_ADDRESS}
 created_database ${RESOURCE_GROUP_APP} ${DATABASE_SERVER_NAME} ${DATABASE_NAME} ${DATABASE_SKU}
-echo -e " ${COLOR_BLUE}DATABASE_URL=${COLOR_GREEN}'sqlserver://${databaseServerFqdn}:1433;database=${DATABASE_NAME};user=${DATABASE_SERVER_ADMIN_USER}@${DATABASE_SERVER_NAME};password=${DATABASE_SERVER_ADMIN_PASSWORD}'${NC}\n"
+echo -e " ${COLOR_BLUE}DATABASE_URL=${COLOR_GREEN}'sqlserver://${databaseServerFqdn}:1433;database=${DATABASE_NAME};user=${DATABASE_SERVER_ADMIN_USER}@${DATABASE_SERVER_NAME};password=REDACTED'${NC}\n"
 create_app_service_plan ${RESOURCE_GROUP_APP} ${SERVICE_PLAN_NAME}
-echo -e "All infrastructure tasks ${COLOR_GREEN}completed successfully!${NC}"
+create_blob_storage ${RESOURCE_GROUP_APP} ${STORAGE_ACCOUNT_NAME} ${STORAGE_ACCOUNT_SKU} ${STORAGE_BLOB_CONTAINER_NAME}
+echo -e "\nAll infrastructure tasks ${COLOR_GREEN}completed successfully!${NC}"
