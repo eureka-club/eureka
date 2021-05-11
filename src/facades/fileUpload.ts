7c537224b8a77b0a732dcccd2a16762b5571b762
srigi <srigi@srigi.sk>
2021-05-11 21:05:45 +0200

    Add Azure storage mechanism for public-assets

--- src/facades/fileUpload.ts
+++ src/facades/fileUpload.ts
@@ -1,12 +1,17 @@
+import { BlobServiceClient, StorageSharedKeyCredential, newPipeline } from '@azure/storage-blob';
 import crypto from 'crypto';
 import fs, { existsSync, mkdirSync } from 'fs';
 import moveFile from 'move-file';
 import path from 'path';
 
 import { FileUpload, StoredFileUpload } from '../types';
+import { STORAGE_MECHANISM_AZURE, STORAGE_MECHANISM_LOCAL_FILES } from '../constants';
 
+const { AZURE_STORAGE_ACCOUNT_ACCESS_KEY } = process.env;
+const { AZURE_STORAGE_ACCOUNT_NAME } = process.env;
 const { LOCAL_ASSETS_HOST_DIR } = process.env;
-const { PUBLIC_ASSETS_STORAGE_MECHANISM } = process.env;
+const { NEXT_PUBLIC_AZURE_STORAGE_ACCOUNT_CONTAINER_NAME } = process.env;
+const { NEXT_PUBLIC_PUBLIC_ASSETS_STORAGE_MECHANISM } = process.env;
 const HASHING_ALGO = 'sha256';
 const FOLDER_SPREAD_CHAR_COUNT = 2;
 
@@ -18,7 +23,14 @@ const getFileHash = (filePath: string, algorithm = HASHING_ALGO): string => {
   return sha1sum.digest('hex');
 };
 
-const moveUploaded = async (fileUpload: FileUpload, fileHash: string): Promise<string> => {
+const getFileStorePath = (fileUpload: FileUpload, fileHash: string): string => {
+  const fileDestDir = path.join(fileHash.substr(0, FOLDER_SPREAD_CHAR_COUNT));
+  const fileDestPath = path.join(fileDestDir, fileHash + path.extname(fileUpload.path));
+
+  return fileDestPath;
+};
+
+const moveLocalUpload = async (fileUpload: FileUpload, fileHash: string): Promise<string> => {
   const fileDestDir = path.join(LOCAL_ASSETS_HOST_DIR!, fileHash.substr(0, FOLDER_SPREAD_CHAR_COUNT));
   if (!existsSync(fileDestDir)) {
     mkdirSync(fileDestDir);
@@ -33,17 +45,36 @@ const moveUploaded = async (fileUpload: FileUpload, fileHash: string): Promise<s
 
 export const storeUpload = async (file: FileUpload): Promise<StoredFileUpload> => {
   const fileHash = getFileHash(file.path);
+  const fileStorePath = getFileStorePath(file, fileHash);
 
-  switch (PUBLIC_ASSETS_STORAGE_MECHANISM) {
-    case 'local': {
-      const fileDestPath = await moveUploaded(file, fileHash);
-      const assetsPathPrefixClean = LOCAL_ASSETS_HOST_DIR!.replace('./', '');
-      const cutLength = (assetsPathPrefixClean + path.delimiter).length;
+  switch (NEXT_PUBLIC_PUBLIC_ASSETS_STORAGE_MECHANISM) {
+    case STORAGE_MECHANISM_AZURE: {
+      const blobServiceClient = new BlobServiceClient(
+        `https://${AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net`,
+        newPipeline(new StorageSharedKeyCredential(AZURE_STORAGE_ACCOUNT_NAME!, AZURE_STORAGE_ACCOUNT_ACCESS_KEY!)),
+      );
+      const containerClient = blobServiceClient.getContainerClient(NEXT_PUBLIC_AZURE_STORAGE_ACCOUNT_CONTAINER_NAME!);
+      const blockBlobClient = containerClient.getBlockBlobClient(fileStorePath);
+
+      await blockBlobClient.uploadFile(file.path, {
+        blobHTTPHeaders: { blobContentType: file.headers['content-type'] },
+      });
 
       return {
         contentHash: fileHash,
         originalFilename: file.originalFilename,
-        storedFile: fileDestPath.substr(cutLength),
+        storedFile: fileStorePath,
+        mimeType: file.headers['content-type'],
+      };
+    }
+
+    case STORAGE_MECHANISM_LOCAL_FILES: {
+      await moveLocalUpload(file, fileHash);
+
+      return {
+        contentHash: fileHash,
+        originalFilename: file.originalFilename,
+        storedFile: fileStorePath,
         mimeType: file.headers['content-type'],
       };
     }
