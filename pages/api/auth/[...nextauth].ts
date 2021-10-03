0c0e396e59f6c174858f6f1ea5ee76a68cefcd6b
geo <gbanoaol@gmail.com>
2021-10-03 11:24:26 -0400

    BF-test if the email login error is because the custom verification mail

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -110,40 +110,48 @@ export default (req: NextApiRequest, res: NextApiResponse): void | Promise<void>
           },
         },
         from: process.env.EMAILING_FROM,
-        sendVerificationRequest: async ({ identifier: email, url, baseUrl }): Promise<void> => {
-          const site = baseUrl.replace(/^https?:\/\//, '');
-          const t = await getT(locale, 'singInMail');
-          const title = t('title');
-          const subtitle = t('subtitle');
-          const singIngConfirmationUrl = t('singIngConfirmationUrl');
-          const ignoreEmailInf = t('ignoreEmailInf');
-          const aboutEureka = t('aboutEureka');
-          const emailReason = t('emailReason');
-          const opt = {
-            to: [
-              {
-                email,
-              },
-            ],
-            from: {
-              email: process.env.EMAILING_FROM!,
-              name: 'EUREKA-CLUB',
-            },
-            subject: `Sign in to ${site} on: ${new Date().toUTCString()}`,
-            html: '',
-          };
+        // sendVerificationRequest: async ({ identifier: email, url, baseUrl }): Promise<void> => {
+        //   const site = baseUrl.replace(/^https?:\/\//, '');
+        //   const t = await getT(locale, 'singInMail');
+        //   const title = t('title');
+        //   const subtitle = t('subtitle');
+        //   const singIngConfirmationUrl = t('singIngConfirmationUrl');
+        //   const ignoreEmailInf = t('ignoreEmailInf');
+        //   const aboutEureka = t('aboutEureka');
+        //   const emailReason = t('emailReason');
+        //   const opt = {
+        //     to: [
+        //       {
+        //         email,
+        //       },
+        //     ],
+        //     from: {
+        //       email: process.env.EMAILING_FROM!,
+        //       name: 'EUREKA-CLUB',
+        //     },
+        //     subject: `Sign in to ${site} on: ${new Date().toUTCString()}`,
+        //     html: '',
+        //   };
+        //   try {
+        //     const emailRes = await sendMailSingIn(opt, {
+        //       to: email,
+        //       url,
+        //       title,
+        //       subtitle,
+        //       singIngConfirmationUrl,
+        //       ignoreEmailInf,
+        //       aboutEureka,
+        //       emailReason,
+        //     });
+        //     if (!emailRes)
 
-          await sendMailSingIn(opt, {
-            to: email,
-            url,
-            title,
-            subtitle,
-            singIngConfirmationUrl,
-            ignoreEmailInf,
-            aboutEureka,
-            emailReason,
-          });
-        },
+        //   }
+        //   catch (e) {
+        //     console.error(JSON.stringify(e));
+        //     console.log(JSON.stringify(e));
+        //     console.dir(e)
+        //   }
+        // },
       }),
     ],
     secret: process.env.SECRET,
