c660b278dc5e9297a78179e5cc20cfd172411f45
geo <gbanoaol@gmail.com>
2021-10-29 11:15:02 -0400

    FE-changed the logo and favicon, added active class to About menu and removed bold style

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,11 +9,11 @@ class MyDocument extends Document {
       <Html>
         <Head>
           {/* favicon */}
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/logo.svg" />
+          <link rel="icon" type="image/svg" sizes="32x32" href="/logo.svg" />
+          <link rel="icon" type="image/svg" sizes="16x16" href="/logo.svg" />
           <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#3cd1b3" />
+          <link rel="mask-icon" href="/logo.svg" color="#3cd1b3" />
           <meta name="msapplication-TileColor" content="#2d89ef" />
           <meta name="theme-color" content="#ffffff" />
 
