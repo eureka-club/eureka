18063ecfcc70e5f07cf7e31cacf7f4470dad403f
Jose Manuel Gallardo <jmgallardo84@gmail.com>
2022-01-27 20:54:33 -0500

    IM-Adding responsivity to comments component for smaller screens

--- src/components/common/CommentCmp.tsx
+++ src/components/common/CommentCmp.tsx
@@ -356,21 +356,21 @@ const CommentCmp: FunctionComponent<Props> = ({ comment, cacheKey }) => {
       {
         /* !isLoading &&  */ comment && (
           <Card key={comment.id} className="mt-1 bg-white border-0">
-            <Row>
-              <Col xs={2} md={1} className="pe-0 text-right">
+            <Row className='d-flex justify-content-center' >
+              <Col xs={2} md={1} className="d-flex justify-content-center justify-content-md-end p-1">
                 <Avatar user={comment.creator} size="xs" showName={false} />
               </Col>
-              <Col xs={10} md={11} className="ps-0">
+              <Col xs={10} md={11} className="d-flex flex-column">
                 <CommentTextBox comment={ comment} />
                 {/* {renderCommentActions()} */}<CommentActionsBar key={ v4()} comment={ comment} cacheKey={cacheKey}/>
                 {comment &&
                   comment.comments &&
                   comment.comments.map((commentChild) => (
-                    <Row key={v4()} className="mb-1">
-                      <Col md={1} xs={2} className="pe-0">
+                    <Row key={v4()} className="mb-2">
+                      <Col md={1} xs={2} className="d-flex justify-content-center justify-content-md-end p-1">
                         <Avatar user={commentChild.creator} size="xs" showName={false} />
                       </Col>
-                      <Col md={11} xs={10} className="ps-0">
+                      <Col md={11} xs={10} className="d-flex flex-column">
                       <CommentTextBox comment={ commentChild} />    
                         {/* {renderCommentActions(commentChild)}                     */}
                         <CommentActionsBar key={ v4()} comment={commentChild} cacheKey={cacheKey}/>
