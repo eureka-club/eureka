fdd4f8b972ffc0dade4fe97a6a8d5cacdc9cd781
Geordanis B. Vega <gbanoaol@gmail.com>
2022-04-19 15:09:49 -0400

    FE-login with mailchimp email

---
+++ pages/api/sendMail.ts
@@ -0,0 +1,40 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  data: boolean|any
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  
+  if(req.method=='POST'){
+    
+    // const r = await send()
+    // res.status(200).json({data:r})
+    const {to,subject,text,html,from_name} = req.body;
+    
+    const message = {
+      from_email: process.env.EMAILING_FROM,
+      from_name: from_name||'EUREKA',
+      subject,
+      ...text && {text},
+      ...html && {html},
+      to
+    };
+    
+    const mailchimp = require('@mailchimp/mailchimp_transactional')(process.env.EMAIL_SERVER_PASS);
+    const response = await mailchimp.messages.send({message});
+    console.log(response);
+    if(response.status == "sent")
+      return res.status(200).json({data:response})
+    res.status(500).json({data:response.reject_reason})
+   
+  }
+  if(req.method=='GET'){
+    res.status(200).json({data:'ok'})
+  }
+
+  
+}
