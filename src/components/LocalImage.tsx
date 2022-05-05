110ec695597431d669e47d104d90fb2a634ceee3
Geordanis B. Vega <gbanoaol@gmail.com>
2022-05-05 08:12:46 -0400

    FE-adding-fallback-img

--- src/components/LocalImage.tsx
+++ src/components/LocalImage.tsx
@@ -18,9 +18,10 @@ interface Props {
 }
 
 const LocalImage: FunctionComponent<Props> = ({ className, style, filePath, alt,width,height,blurDataURL=false,notNextImage }) => {
-  const fallbakImgURL = '/img/ico-painting.png'
+  const fallbakImgURL = `https://${NEXT_PUBLIC_AZURE_CDN_ENDPOINT}.azureedge.net/${NEXT_PUBLIC_AZURE_STORAGE_ACCOUNT_CONTAINER_NAME}/Image-not-found.webp`
   const [imgError,setImgError] = useState<boolean>(false)
   const onLoadImgError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    e.currentTarget.src = fallbakImgURL;
     setImgError(true)
   };
 
@@ -48,6 +49,7 @@ const LocalImage: FunctionComponent<Props> = ({ className, style, filePath, alt,
           src={`https://${NEXT_PUBLIC_AZURE_CDN_ENDPOINT}.azureedge.net/${NEXT_PUBLIC_AZURE_STORAGE_ACCOUNT_CONTAINER_NAME}/${filePath}`}
           alt={alt}
           className={className}
+          onError={onLoadImgError}
           style={{...style,width:`${width}px`,height:`${height}px`}}
          />
       );
