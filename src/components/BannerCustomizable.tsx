9d02a960183dd04a3adc3148ba1e998e22e19ac0
Jose Manuel Gallardo <jmgallardo84@gmail.com>
2022-06-10 14:44:50 -0400

    Working on Custom banner on explore page

--- src/components/BannerCustomizable.tsx
+++ src/components/BannerCustomizable.tsx
@@ -3,7 +3,7 @@ import { Button, Container,Carousel } from 'react-bootstrap';
 import { AiOutlineClose, AiOutlineDown } from 'react-icons/ai';
 import useTranslation from 'next-translate/useTranslation';
 import { useQueryClient } from 'react-query';
-import styles from './Banner.module.css';
+import styles from './BannerCustomizable.module.css';
 import Banner from '@/src/components/Banner';
 
 
@@ -36,7 +36,7 @@ const BannerCustomizable: FunctionComponent = ({
 
   return (
     <>
-      <section className="bg-primary p-3" style={{ backgroundImage: "url('/img/bg-header.svg')",marginTop:'4.8em' }}>
+      <section className="d-block bg-primary p-3" style={{ backgroundImage: "url('/img/bg-header.svg')",marginBottom:(show) ? '5em' : '2em',marginTop:'4.8em',maxHeight: (show) ? '16em' : '4em' }}>
         <aside className="d-flex justify-content-end">
           {show && (
             <Button
@@ -53,49 +53,72 @@ const BannerCustomizable: FunctionComponent = ({
               onClick={() => setShow(true)}
               className="py-1 px-3 border-white text-white fs-6 bg-transparent rounded-pill"
             >
-              {t('openHeaderLabel')} <AiOutlineDown />
+              {t('Expand')} <AiOutlineDown />
             </Button>
           )}
         </aside>
         {show && (
           <>
-            <Container className={`${styles.grid} text-white text-center`}>
-              <h1 className="h1 ms-auto mt-3">{t('underEurekaLabel')}</h1>
-              <p className="">
-                {t('eurekaReasonLabel')}
-                <br />
-                {t('eurekaReasonLabel2')}
-              </p>
+            <Container className={`${styles.grid} text-white text-center`} style={{minHeight:'1em'}}>             
             </Container>
-            <Carousel style={{ marginBottom: '-5em' }} nextIcon={<em />} prevIcon={<em />}>
-              <Carousel.Item className="p-3 text-white mb-1 bg-secondary" style={{ height: '16rem' }}>
-                <aside className="d-block w-100">
-                  <div className="d-flex justify-content-center">
-                    <aside className={`${styles.bgHeaderLeft}`} />
+            <div className='d-flex justify-content-center'>
+            <Carousel indicators={true} controls={false}  style={{width: '65%'}} >
+              <Carousel.Item className="p-3 text-white mb-1 bg-secondary" style={{ marginBottom:'',height: '14rem' }}>
+                  <div className="d-flex flex-row" style={{maxWidth:'92%', maxHeight: '12rem' }}>
+                    <div className='' >
+                         <img
+                          style={{width:'12em',height:'12em'}}
+                          src="/img/default-avatar.png"
+                          alt=""
+                        />
+                    </div>
+                    <div className="d-flex flex-column justify-content-center" style={{minWidth:'85%'}}>
+                      <h2 className="h1 text-center">  Title Slider1 Title Slider1</h2>
+                      <p className="p-0 mx-1 text-wrap text-center fs-4 fst-italic">Text Slider1 Text Slider1 Text Slider1 Text Slider1  Text Slider1 Text Slider1 Text Slider1 Text Slider1</p>
+                       <div className="d-flex justify-content-end me-1"> 
+                          <Button variant="primary" className='text-white' >{t('JoinEureka')}</Button>
+                      </div>
+                    </div>
                   </div>
-                  <h2 className="h6 text-center">{t('headerLeftBoxTitle')}</h2>
-                  <p className="p-0 m-0 text-wrap text-center fs-6 fst-italic">{t('headerLeftBoxText')}</p>
-                </aside>
               </Carousel.Item>
-              <Carousel.Item className="p-3 text-white mb-1 bg-secondary" style={{ height: '16rem' }}>
-                <aside className="d-block w-100">
-                  <div className="d-flex justify-content-center">
-                    <aside className={`${styles.bgHeaderCenter}`} />
+              <Carousel.Item className="p-3 text-white mb-1 bg-secondary" style={{ height: '14rem' }}>
+                <div className="d-flex flex-row" style={{maxWidth:'92%', maxHeight: '12rem' }}>
+                    <div className='' >
+                         <img
+                          style={{width:'12em',height:'12em'}}
+                          src="/img/default-avatar.png"
+                          alt=""
+                        />
+                    </div>
+                    <div className="d-flex flex-column justify-content-center" style={{minWidth:'85%'}}>
+                      <h2 className="h1 text-center">  Title Slider2 Title Slider2</h2>
+                      <p className="p-0 mx-1 text-wrap text-center fs-4 fst-italic">Text Slider2 Text Slider2 Text Slider2 Text Slider2  Text Slider2 Text Slider2 Text Slider2 Text Slider2</p>
+                       <div className="d-flex justify-content-end me-1"> 
+                          <Button variant="primary" className='text-white' >{t('JoinEureka')}</Button>
+                      </div>
+                    </div>
                   </div>
-                  <h2 className="h6 text-center">{t('headerCenterBoxTitle')}</h2>
-                  <p className="p-0 m-0 text-wrap text-center fs-6 fst-italic">{t('headerCenterBoxText')}</p>
-                </aside>
               </Carousel.Item>
-              <Carousel.Item className="p-3 text-white mb-1 bg-secondary" style={{ height: '16rem' }}>
-                <aside className="d-block w-100">
-                  <div className="d-flex justify-content-center">
-                    <aside className={`${styles.bgHeaderRight}`} />
+              <Carousel.Item className="p-3 text-white mb-1 bg-secondary" style={{ height: '14rem' }}>
+                <div className="d-flex flex-row" style={{maxWidth:'92%', maxHeight: '12rem' }}>
+                    <div className='' >
+                         <img
+                          style={{width:'12em',height:'12em'}}
+                          src="/img/default-avatar.png"
+                          alt=""
+                        />
+                    </div>
+                    <div className="d-flex flex-column justify-content-center" style={{minWidth:'85%'}}>
+                      <h2 className="h1 text-center">  Title Slider 3 Title Slider 3</h2>
+                      <p className="p-0 mx-1 text-wrap text-center fs-4 fst-italic">Text Slider3 Text Slider3 Text Slider3 Text Slider3  Text Slider3 Text Slider3 Text Slider3 Text Slider3</p>
+                       <div className="d-flex justify-content-end me-1"> 
+                          <Button variant="primary" className='text-white' >{t('JoinEureka')}</Button>
+                      </div>
+                    </div>
                   </div>
-                  <h2 className="h6 text-center">{t('headerRightBoxTitle')}</h2>
-                  <p className="p-0 m-0 text-wrap text-center fs-6 fst-italic">{t('headerRightBoxText')}</p>
-                </aside>
               </Carousel.Item>
             </Carousel>
+            </div>
           </>
         )}
       </section>
