f8ec6a393d62a1a317697ec5c7d06008720c263b
Geordanis B. Vega <geo@xs-MacBook-Air.local>
2022-07-26 14:53:06 -0400

    fixing conflicts

--- next.config.js
+++ next.config.js
@@ -5,30 +5,9 @@ const domain = process.env.NODE_ENV !== 'production'
     ? process.env.CDN_ENDPOINT_NAME_STAGING
     : process.env.CDN_ENDPOINT_NAME_PRODUCTION;
 
-<<<<<<< HEAD
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-  
-module.exports = withBundleAnalyzer({
-  ...nextTranslate(),
-  sassOptions: {
-      includePaths: [path.join(__dirname, 'scss')],
-  },
-  env: {
-      NEXT_PUBLIC_SITE_NAME:"Eureka",
-      NEXT_PUBLIC_LOCAL_ASSETS_BASE_URL:"/assets"
-  },
-  images:{
-      domains:[`${domain}.azureedge.net`],
-  },
-  
-})
-
-=======
 module.exports = {
   experimental: {
-    outputStandalone: true,
+     outputStandalone: true,
   },
   output: 'standalone',
     ...nextTranslate(),
@@ -42,18 +21,4 @@ module.exports = {
     images:{
         domains:[`${domain}.azureedge.net`],
     },
-    async headers() {
-        return [
-          {
-            source: '/resetPass',
-            headers: [
-              {
-                key: 'gmail',
-                value: 'my custom header value',
-              },
-            ],
-          },
-        ]
-    },
 };
->>>>>>> develop
