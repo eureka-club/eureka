c4b6d070996ac94a38c9236869a402c0681ce52a
Geordanis B. Vega <geo@xs-MacBook-Air.local>
2022-07-30 19:06:45 -0400

    IM-improving the redirect for wrongs api request urls generated by next-translate

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,14 @@
-// middleware.ts
 import { NextRequest, NextResponse } from 'next/server';
 
-// If the incoming request has the "beta" cookie
-// then we'll rewrite the request to /beta
 export function middleware(req: NextRequest) {
-  debugger;
   if(req.nextUrl.pathname.match(/undefined\/api/g)){
-    const url = req.nextUrl.pathname.replace("/undefined","")
+    const href = req.nextUrl.href;
+    const url = href.replace("/undefined","");
     return NextResponse.redirect(url)
   }
   return NextResponse.rewrite(req.nextUrl);
 }
 
 export const config = {
-  matcher: '/undefined',
+  matcher: '/undefined/api/:path*',
 };
