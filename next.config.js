d614cc6dcbe33506abc0c5dbffe2f72b832f390c
Geordanis B. Vega <geo@xs-MacBook-Air.local>
2022-08-01 12:48:01 -0400

    FE-test deploy with azure github actions

--- next.config.js
+++ next.config.js
@@ -5,56 +5,7 @@ const domain = process.env.NODE_ENV !== 'production'
     ? process.env.CDN_ENDPOINT_NAME_STAGING
     : process.env.CDN_ENDPOINT_NAME_PRODUCTION;
 
-// module.exports = nextTranslate({
-//     sassOptions: {
-//         includePaths: [path.join(__dirname, 'scss')],
-//     },
-//     env: {
-//         NEXT_PUBLIC_SITE_NAME:"Eureka",
-//         NEXT_PUBLIC_LOCAL_ASSETS_BASE_URL:"/assets"
-//     },
-//     images:{
-//         domains:[`${domain}.azureedge.net`],
-//     },
-//     async redirects() {
-//         const destination = `${process.env.NEXT_PUBLIC_WEBAPP_URL}/:path*`;
-//         const res = [
-//             {
-//                 source: '/undefined/:path*',
-//                 destination,
-//                 permanent: false,
-//             },
-//             {
-//                 source: '/en/undefined/:path*',
-//                 destination,
-//                 permanent: false,
-//             },
-//             {
-//                 source: '/es/undefined/:path*',
-//                 destination,
-//                 permanent: false,
-//             },
-//             {
-//                 source: '/fr/undefined/:path*',
-//                 destination,
-//                 permanent: false,
-//             },
-//             {
-//                 source: '/pt/undefined/:path*',
-//                 destination,
-//                 permanent: false,
-//             },
-            
-            
-//         ]
-//         console.log(res)
-//         return res;
-//       },
-//     output: 'standalone'
-//   })
-
-
-module.exports = {
+module.exports = nextTranslate({
     sassOptions: {
         includePaths: [path.join(__dirname, 'scss')],
     },
@@ -65,6 +16,39 @@ module.exports = {
     images:{
         domains:[`${domain}.azureedge.net`],
     },
-    output: 'standalone'
-
-}
+    // async redirects() {
+    //     const destination = `${process.env.NEXT_PUBLIC_WEBAPP_URL}/:path*`;
+    //     const res = [
+    //         {
+    //             source: '/undefined/:path*',
+    //             destination,
+    //             permanent: false,
+    //         },
+    //         {
+    //             source: '/en/undefined/:path*',
+    //             destination,
+    //             permanent: false,
+    //         },
+    //         {
+    //             source: '/es/undefined/:path*',
+    //             destination,
+    //             permanent: false,
+    //         },
+    //         {
+    //             source: '/fr/undefined/:path*',
+    //             destination,
+    //             permanent: false,
+    //         },
+    //         {
+    //             source: '/pt/undefined/:path*',
+    //             destination,
+    //             permanent: false,
+    //         },
+            
+            
+    //     ]
+    //     console.log(res)
+    //     return res;
+    //   },
+    // output: 'standalone'
+  })
