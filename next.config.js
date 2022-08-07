91e31fe36404e3c55d176d98a330028a8d06bf4c
Geordanis B. Vega <geo@xs-MacBook-Air.local>
2022-08-07 08:59:22 -0400

    fe/testing ci

--- next.config.js
+++ next.config.js
@@ -2,27 +2,13 @@ const nextTranslate = require('next-translate');
 const path = require('path');
 
 const isProduction = process.env.NODE_ENV == 'production'
-const domain = !isProduction
-    ? process.env.CDN_ENDPOINT_NAME_STAGING
-    : process.env.CDN_ENDPOINT_NAME_PRODUCTION;
-
-const NEXT_PUBLIC_AZURE_CDN_ENDPOINT = isProduction 
- ? process.env.NEXT_PUBLIC_AZURE_CDN_ENDPOINT
- : NEXT_PUBLIC_AZURE_CDN_ENDPOINT_STAGING
 
 module.exports = nextTranslate({
     sassOptions: {
         includePaths: [path.join(__dirname, 'scss')],
     },
-    env: {
-        NEXT_PUBLIC_SITE_NAME:"Eureka",
-        NEXT_PUBLIC_LOCAL_ASSETS_BASE_URL:"/assets",
-        NEXT_PUBLIC_AZURE_CDN_ENDPOINT,
-        NEXT_PUBLIC_AZURE_STORAGE_ACCOUNT_CONTAINER_NAME: process.env.NEXT_PUBLIC_AZURE_STORAGE_ACCOUNT_CONTAINER_NAME,
-        NEXT_PUBLIC_PUBLIC_ASSETS_STORAGE_MECHANISM:process.env.NEXT_PUBLIC_PUBLIC_ASSETS_STORAGE_MECHANISM
-    },
     images:{
-        domains:[`${domain}.azureedge.net`],
+        domains:[`${process.env.CDN_ENDPOINT_NAME}.azureedge.net`],
     },
     // async redirects() {
     //     const destination = `${process.env.NEXT_PUBLIC_WEBAPP_URL}/:path*`;
@@ -58,5 +44,5 @@ module.exports = nextTranslate({
     //     console.log(res)
     //     return res;
     //   },
-        output: 'standalone'
+    output: 'standalone'
   })
