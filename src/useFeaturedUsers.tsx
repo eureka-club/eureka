73383ec486c8d779f9dabc8d7469aceef5a6b0a0
Jose Manuel Gallardo <jmgallardo84@gmail.com>
2022-08-11 16:10:20 -0400

    Addinf featured users carousel

---
+++ src/useFeaturedUsers.tsx
@@ -0,0 +1,25 @@
+import useUsers,{getUsers} from './useUsers';
+import useBackOffice from '@/src/useBackOffice';
+
+const featuredUsersWhere = (ids:number[]) => ({
+  where:{
+    id: { in: ids },
+  }
+}) 
+
+export const getFeaturedUsers = async (ids:number[],take:number=8)=>{
+  return getUsers({...featuredUsersWhere(ids),take});
+}
+
+const useFeaturedUsers = () => {
+  const {data:bo} = useBackOffice();
+  let usersIds:number[] = [];
+  if(bo && bo.FeaturedUsers)
+    bo.FeaturedUsers.split(',').forEach(x=> usersIds.push(parseInt(x)));
+      
+  return useUsers(featuredUsersWhere(usersIds),
+    {enabled:!!usersIds}
+  )
+};
+
+export default useFeaturedUsers;
