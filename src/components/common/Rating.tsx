5f02a7325bb3487ea7143d6f88669708b7085a7a
Geordanis B. Vega <geo@xs-MacBook-Air.local>
2022-08-28 18:24:03 -0400

    im-removing useState for useReducer

--- src/components/common/Rating.tsx
+++ src/components/common/Rating.tsx
@@ -8,15 +8,34 @@ interface Props {
  onClick:(value:number)=>void;
  readonly?:boolean;
 }
+enum ActionTypes{
+    'set_value'
+}
+interface Action{
+    type:ActionTypes;
+    payload:Record<string,any>;
+}
+interface State{
+    value:number;
+}
+const reducer = (state:State,action:Action)=>{
+    switch(action.type){
+        case ActionTypes.set_value:
+            return {
+                ...state,
+                value:action.payload.value
+            }
+        default: return state;
+    }
+}
 
 const Rating:React.FC<Props> = ({stop:s1,qty,onClick:ock,readonly})=>{
     const stop = s1 || 5;
-    const [value,setValue]=useState(qty);
+    const initialState = {
+        value:qty
+    }
+    const [state,dispatch] = useReducer(reducer,initialState)
 
-    useEffect(()=>{
-        setValue(qty)
-    },[qty])
-    
     const renderValues = ()=>{
         let res = [];
         const onClick = (e:React.MouseEvent<SVGElement, MouseEvent>,val:number)=>{
@@ -27,16 +46,18 @@ const Rating:React.FC<Props> = ({stop:s1,qty,onClick:ock,readonly})=>{
             }
         }
         const onMouseEnter = (e:React.MouseEvent<SVGElement, MouseEvent>,val:number)=>{
-            if(!readonly)setValue(val)
+            if(!readonly)
+                dispatch({type:ActionTypes.set_value,payload:{value:val}})
         }
         const onMouseLeave = (e:React.MouseEvent<SVGElement, MouseEvent>)=>{
-            if(!readonly)setValue(qty)
+            if(!readonly)
+                dispatch({type:ActionTypes.set_value,payload:{value:qty}})
         }
         for(let i =0;i<stop;i++)
             res.push(
                     <GiBrain 
                         role={'button'}
-                        className={`fs-6 cursor-pointer ${i<value ? styles.rated : styles.notRated}`} 
+                        className={`fs-6 cursor-pointer ${i<state.value ? styles.rated : styles.notRated}`} 
                         onClick={(e)=>onClick(e,i+1)}  
                         onMouseEnter={(e)=>onMouseEnter(e,i+1)}
                         onMouseLeave={onMouseLeave}
