3970d7470efa5bbc0a6b5258cf907a5d0261face
Geordanis B. Vega <geo@xs-MacBook-Air.local>
2022-08-30 18:12:39 -0400

    fe-refactorization in the component FilterEngineCountries to be used in all filters in the tabs

---
+++ src/components/FilterEngineCountries.tsx
@@ -0,0 +1,151 @@
+import React, { useState,ChangeEvent, useEffect,Dispatch,SetStateAction  } from "react"
+import styles from './FilterEngine.module.css';
+import { Country } from "../types";
+import useTranslation from 'next-translate/useTranslation';
+
+import { Form, OverlayTrigger, Popover, Button } from 'react-bootstrap';
+import TagsInputTypeAhead from './forms/controls/TagsInputTypeAhead';
+import useCountries from '@/src/useCountries'
+interface Props{
+  filtersCountries:string[];
+  setFiltersCountries: Dispatch<SetStateAction<string[]>>;
+  filtersRegions:Record<string,boolean>;
+  setFiltersRegions: Dispatch<SetStateAction<Record<string,boolean>>>;
+}
+const FilterEngineCountries:React.FC<Props> = ({filtersCountries,setFiltersCountries,filtersRegions,setFiltersRegions})=>{
+    const { t } = useTranslation('searchEngine');
+
+    const [fc,setFC]=useState<string[]>(filtersCountries)
+    const [fr,setFR]=useState<Record<string,boolean>>(filtersRegions)
+
+    const handlerComboxesChangeRegions = (e: ChangeEvent<HTMLInputElement>, q: string) => {
+      const parentCode = q;
+      const rch = {...fr, [`${parentCode}`]: e.target.checked};
+      setFR(rch)
+      const countriesByRegion = countriesAll.filter(c=>c.parentCode == parentCode).map(i=>i.code)
+
+      if(e.target.checked){
+        setFC(res=>[...res,...countriesByRegion]);
+      }
+      else{
+        const countries = fc.filter(c=>!countriesByRegion.includes(c))
+        setFC(countries);
+
+      }
+    };
+  
+    const {data} = useCountries()
+    const [countriesAll,setCountriesAll] = useState<{code:string,label:string,parentCode:string}[]>([]);
+    useEffect(()=>{
+      if(data)setCountriesAll(data.map((d:Country)=>({code:d.code,label:d.code,parentCode:d.parent!.code})))
+    },[data])
+    
+    const getPopoverGeography = () => {
+      return <Popover id='popover-geography' className="position-absolute top-0">
+        <Popover.Body>
+              <div>
+                <Form.Label>
+                        <strong>{t('Regions')}</strong>
+                </Form.Label>
+                <Form.Group className={styles.formGroup}>
+                  <Form.Check
+                    className={styles.filter}
+                    type="checkbox"
+                    label={`${t('countries:Asia')} `}
+                    checked={fr.Asia}
+                    onChange={(e) => handlerComboxesChangeRegions(e, 'Asia')}
+                  />
+                </Form.Group>
+                <Form.Group className={styles.formGroup}>
+                  <Form.Check
+                    className={styles.filter}
+                    type="checkbox"
+                    label={`${t('countries:Europe')} `}
+                    checked={fr.Europe}
+                    onChange={(e) => handlerComboxesChangeRegions(e, 'Europe')}
+                  />
+                </Form.Group>
+                <Form.Group className={styles.formGroup}>
+                  <Form.Check
+                    className={styles.filter}
+                    type="checkbox"
+                    label={`${t('countries:Latin America and the Caribbean')} `}
+                    checked={fr['Latin America and the Caribbean']}
+                    onChange={(e) => handlerComboxesChangeRegions(e, 'Latin America and the Caribbean')}
+                  />
+                </Form.Group>
+                <Form.Group className={styles.formGroup}>
+                  <Form.Check
+                    className={styles.filter}
+                    type="checkbox"
+                    label={`${t('countries:Middle East and North Africa')} `}
+                    checked={fr['Middle East and North Africa']}
+                    onChange={(e) => handlerComboxesChangeRegions(e, 'Middle East and North Africa')}
+                  />
+                </Form.Group>
+                <Form.Group className={styles.formGroup}>
+                  <Form.Check
+                    className={styles.filter}
+                    type="checkbox"
+                    label={`${t('countries:Northern America')} `}
+                    checked={fr['Northern America']}
+                    onChange={(e) => handlerComboxesChangeRegions(e, 'Northern America')}
+                  />
+                </Form.Group>
+                <Form.Group className={styles.formGroup}>
+                  <Form.Check
+                    className={styles.filter}
+                    type="checkbox"
+                    label={`${t('countries:Oceania')}`}
+                    checked={fr.Oceania}
+                    onChange={(e) => handlerComboxesChangeRegions(e, 'Oceania')}
+                  />
+                </Form.Group>
+                <Form.Group className={styles.formGroup}>
+                  <Form.Check
+                    className={styles.filter}
+                    type="checkbox"
+                    label={`${t('countries:Sub-Saharan Africa')}`}
+                    checked={fr['Sub-Saharan Africa']}
+                    onChange={(e) => handlerComboxesChangeRegions(e, 'Sub-Saharan Africa')}
+                  />
+                </Form.Group>
+
+              </div>
+              <div>
+                <Form.Label>
+                  <strong>{t('Countries')}</strong>
+                </Form.Label>
+                <TagsInputTypeAhead
+                  data={countriesAll}
+                  items={fc}
+                  setItems={setFC}
+                  max={50}
+                  labelKey={(res) => `${t(`countries:${res.code}`)}`}
+                />
+
+              </div>
+              <Button className="my-3" onClick={()=>{
+                setFiltersCountries(fc)
+                setFiltersRegions(fr)
+                }}>{t('common:select')}
+              </Button>
+        </Popover.Body>
+      </Popover>
+    };
+  
+
+    // return <>
+    //     <Button className="ms-3" variant="light" onClick={
+    //       ()=>{
+    //         show(getPopoverGeography(),'ver asd')
+    //       }
+    //     }>{t('Geography')}</Button>
+
+    // </>
+    return <OverlayTrigger trigger="click" placement="bottom" overlay={getPopoverGeography()}>
+        <Button className="ms-3" variant="light">{t('Geography')}</Button>
+      </OverlayTrigger>
+
+  }
+  export default FilterEngineCountries;
--- src/components/useFilterEngineCycles.tsx
+++ src/components/useFilterEngineCycles.tsx
@@ -1,170 +1,24 @@
-import React, { useState,ChangeEvent, useEffect  } from "react"
-import styles from './FilterEngine.module.css';
-import { Country } from "../types";
+import React, { useState,ChangeEvent  } from "react"
 import useTranslation from 'next-translate/useTranslation';
 
-import { Container, Row, Col, Form, OverlayTrigger, Popover, Button } from 'react-bootstrap';
-import TagsInputTypeAhead from './forms/controls/TagsInputTypeAhead';
+import { Row, Col, Form } from 'react-bootstrap';
 
-import useCountries from '@/src/useCountries'
+import FilterEngineCountries from '@/components/FilterEngineCountries'
 
-interface Props{
-  // filtersTypeChanged?: (filtersType:Record<string,boolean>)=>Promise<void>;
-}
 const useFilterEngineCycles = ()=>{
   const { t } = useTranslation('searchEngine');
-  // const {filtersTypeChanged} = props;
-
     const [filtersType,setFiltersType]=useState<Record<string,boolean>>({private:true,public:true})
-    const [filtersRegions,setFiltersRegions]=useState<Record<string,boolean>>({})
-    const [filtersCountries,setFiltersCountries]=useState<string[]>([])
-
-    const [onlyCountries,setOnlyCountries]=useState<string[]>([])
-
-
-    const handlerComboxesChangeRegions = (e: ChangeEvent<HTMLInputElement>, q: string) => {
-      const fc = {...filtersRegions, [`${q}`]: e.target.checked};
-      // let c = data.filter(i=>{
-      //   if(i.parent.code == q)return true;
-      // })
-      
-      console.log(countriesAll,q)
-      setFiltersRegions(fc);
-      console.log(fc)
-    };
     
+    const [filtersCountries,setFiltersCountries]=useState<string[]>([])
+    const [filtersRegions,setFiltersRegions]=useState<Record<string,boolean>>({})
+
     const handlerComboxesChangeType = (e: ChangeEvent<HTMLInputElement>, q: string) => {
       const fc = {...filtersType, [`${q}`]: e.target.checked};
       setFiltersType(fc);
       // if(filtersTypeChanged)
       //   filtersTypeChanged(fc)
     };
-  
-    const {data} = useCountries([...onlyCountries])
-    const [countries,setCountries] = useState<string[]>([]);
-    const [countriesAll,setCountriesAll] = useState<{code:string,label:string,parentCode:string}[]>([]);
-    useEffect(()=>{
-      if(data)setCountriesAll(data.map((d:Country)=>({code:d.code,label:d.code,parentCode:d.parent!.code})))
-    },[data])
-
-    const getPopoverGeography = () => {
-      return <Popover id='popover-geography' className="position-absolute top-0">
-        <Popover.Body>
-        <Form.Label>
-                      <strong>{t('Regions')}</strong>
-                    </Form.Label>
-                    <Form.Group className={styles.formGroup}>
-                      <Form.Check
-                        className={styles.filter}
-                        type="checkbox"
-                        label={`${t('countries:Asia')} `}
-                        checked={filtersRegions.Asia}
-                        onChange={(e) => handlerComboxesChangeRegions(e, 'Asia')}
-                      />
-                    </Form.Group>
-                    <Form.Group className={styles.formGroup}>
-                      <Form.Check
-                        className={styles.filter}
-                        type="checkbox"
-                        label={`${t('countries:Europe')} `}
-                        checked={filtersRegions.Europe}
-                        onChange={(e) => handlerComboxesChangeRegions(e, 'Europe')}
-                      />
-                    </Form.Group>
-                    <Form.Group className={styles.formGroup}>
-                      <Form.Check
-                        className={styles.filter}
-                        type="checkbox"
-                        label={`${t('countries:Latin America and the Caribbean')} `}
-                        checked={filtersRegions['Latin America and the Caribbean']}
-                        onChange={(e) => handlerComboxesChangeRegions(e, 'Latin America and the Caribbean')}
-                      />
-                    </Form.Group>
-                    <Form.Group className={styles.formGroup}>
-                      <Form.Check
-                        className={styles.filter}
-                        type="checkbox"
-                        label={`${t('countries:Middle East and North Africa')} `}
-                        checked={filtersRegions['Middle East and North Africa']}
-                        onChange={(e) => handlerComboxesChangeRegions(e, 'Middle East and North Africa')}
-                      />
-                    </Form.Group>
-                    <Form.Group className={styles.formGroup}>
-                      <Form.Check
-                        className={styles.filter}
-                        type="checkbox"
-                        label={`${t('countries:Northern America')} `}
-                        checked={filtersRegions['Northern America']}
-                        onChange={(e) => handlerComboxesChangeRegions(e, 'Northern America')}
-                      />
-                    </Form.Group>
-                    <Form.Group className={styles.formGroup}>
-                      <Form.Check
-                        className={styles.filter}
-                        type="checkbox"
-                        label={`${t('countries:Oceania')}`}
-                        checked={filtersRegions.Oceania}
-                        onChange={(e) => handlerComboxesChangeRegions(e, 'Oceania')}
-                      />
-                    </Form.Group>
-                    <Form.Group className={styles.formGroup}>
-                      <Form.Check
-                        className={styles.filter}
-                        type="checkbox"
-                        label={`${t('countries:Sub-Saharan Africa')}`}
-                        checked={filtersRegions['Sub-Saharan Africa']}
-                        onChange={(e) => handlerComboxesChangeRegions(e, 'Sub-Saharan Africa')}
-                      />
-                    </Form.Group>
-                    <br />
-                    <Form.Label>
-                      <strong>{t('Countries')}</strong>
-                    </Form.Label>
-                    {/* <TagsInputTypeAhead data={countries} items={items} tags={tags} setTags={setTags} /> */}
-                    <TagsInputTypeAhead
-                      data={countriesAll}
-                      items={filtersCountries}
-                      setItems={setFiltersCountries}
-                      max={5}
-                      labelKey={(res) => `${t(`countries:${res.code}`)}`}
-                      // onTagCreated={() => {debugger;
-                      //   setFiltersCountries(countries)
-                      //   // const onlyByCountries = [
-                      //   //   ...new Set([...(globalSearchEngineState.onlyByCountries || []), ...items]),
-                      //   // ];
-                      //   // setGlobalSearchEngineState({
-                      //   //   ...globalSearchEngineState,
-                      //   //   ...{ onlyByCountries },
-                      //   // });
-                      // }}
-                      // onTagDeleted={(code) => {
-                      //   // const onlyByCountries = [
-                      //   //   ...new Set([...(globalSearchEngineState.onlyByCountries || []), ...items]),
-                      //   // ];
-                      //   // const idxOBC = onlyByCountries.findIndex((i: string) => i === code);
-                      //   // onlyByCountries.splice(idxOBC, 1);
-                      //   // setGlobalSearchEngineState({
-                      //   //   ...globalSearchEngineState,
-                      //   //   ...{ onlyByCountries },
-                      //   // });
-
-                      // }}
-                    />
-        </Popover.Body>
-      </Popover>
-    };
-  
-
-    // const handlerComboxesChangeType = (e: ChangeEvent<HTMLInputElement>, type: string) => {
-    //     e.stopPropagation();
-    //     const types = type.split('|');
-    //     types.forEach((ty: string) => {
-    //         setRegionsChecked((res) => ({ ...res, [`${ty}`]: !res[`${ty}`] }));
-    //     });
-    //        console.log(filtersChecked) 
-    //   };
     
-const [seeGeographyFilter,setSeeGeographyFilter] = useState(false)
 const FilterEngineCycles: React.FC = ()=>{
     return <Row>
     <Col className="my-3">
@@ -183,12 +37,15 @@ const FilterEngineCycles: React.FC = ()=>{
     
   </Col>
     <Col >
-
-                <Button className="ms-3" variant="light" onClick={()=>setSeeGeographyFilter(r=>!r)}>{t('Geography')}</Button>
-                {seeGeographyFilter && <div className="position-relative">{getPopoverGeography()}</div>}
+    <FilterEngineCountries 
+      filtersCountries={filtersCountries}
+      setFiltersCountries={setFiltersCountries}
+      filtersRegions={filtersRegions}
+      setFiltersRegions={setFiltersRegions}
+    />
     </Col>
     </Row>
   }
-  return {FilterEngineCycles,filtersType,filtersRegions,filtersCountries};
+  return {FilterEngineCycles,filtersType,filtersCountries};
 }
 export default useFilterEngineCycles
