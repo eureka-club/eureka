3e40cf7afc67d51e267dfd0b9e01a4819e632a78
Geordanis B. Vega <gbanoaol@gmail.com>
2022-09-08 16:09:20 -0400

    im-tests-for-online-home-page

--- cypress/e2e/onlineHomePage/index.spec.cy.ts
+++ cypress/e2e/onlineHomePage/index.spec.cy.ts
@@ -9,7 +9,7 @@ describe('Online home page',()=>{
     })
     it('should contains main elements',()=>{
         cy.intercept('/api/auth/session').as('sessionReq')
-        cy.wait('@sessionReq').then(inter=>{
+        cy.wait('@sessionReq',{timeout:30000}).then(inter=>{
             cy.wrap(inter).its('response').its('body').its('user').then(u=>{
                 expect(u.email).to.be.eq('gbanoaol@gmail.com')
             })
@@ -21,7 +21,7 @@ describe('Online home page',()=>{
         const url = `/api/cycle?props=*`
         cy.intercept({method:'GET',url}).as('cyclesCreatedOrJoinedReq')
         
-        cy.wait('@cyclesCreatedOrJoinedReq').then(inter=>{
+        cy.wait('@cyclesCreatedOrJoinedReq',{timeout:30000}).then(inter=>{
             cy.contains('Cycles I created or joined')
             cy.wrap(inter).its('response.body').then(body=>{
                 console.log(body,'body')
