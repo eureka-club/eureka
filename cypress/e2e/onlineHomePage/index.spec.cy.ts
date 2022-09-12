d20e5e8a5b1aeb15b82cfe321c86690b6ff03e46
Geordanis B. Vega <gbanoaol@gmail.com>
2022-09-12 11:24:09 -0400

    bf-error-on-regexp-in-interception-url

--- cypress/e2e/onlineHomePage/index.spec.cy.ts
+++ cypress/e2e/onlineHomePage/index.spec.cy.ts
@@ -1,12 +1,14 @@
 describe('Online home page',()=>{
-    it('should login works',()=>{
+    beforeEach(()=>{
         cy.visit('/en')
         cy.get('[data-cy="login-form"]').within(()=>{
             cy.get('[type="email"]').type('gbanoaol@gmail.com',{force:true})
             cy.get('[type="password"]').type('gbanoaol@gmail.com1',{force:true})
             cy.get('[data-cy="btn-login"]').click({force:true})
         })
+
     })
+    
     it('should contains main elements',()=>{
         cy.intercept('/api/auth/session').as('sessionReq')
         cy.wait('@sessionReq',{timeout:30000}).then(inter=>{
@@ -14,25 +16,43 @@ describe('Online home page',()=>{
                 expect(u.email).to.be.eq('gbanoaol@gmail.com')
             })
         })
-
     })
     it('should have a carousel for "Cycles I created or joined"', ()=>{
-        
-        const url = `/api/cycle?props=*`
-        cy.intercept({method:'GET',url}).as('cyclesCreatedOrJoinedReq')
-        
-        cy.wait('@cyclesCreatedOrJoinedReq',{timeout:30000}).then(inter=>{
-            cy.contains('Cycles I created or joined')
-            cy.wrap(inter).its('response.body').then(body=>{
-                console.log(body,'body')
-                cy.wrap(body).should('have.a.property','data')
+        cy.intercept('/api/cycle\?props=*').as('cyclesCreatedOrJoinedReq')
+        cy.get('[data-cy="myCycles"]')
 
-                cy.get('[data-cy="myCycles"]').within(()=>{
-                    cy.get('[data-cy^="mosaic-item-cycle-"]').should('have.lengthOf',body.data.length)
-                })
+        cy.wait('@cyclesCreatedOrJoinedReq',{timeout:30000}).then((inter)=>{
+            cy.contains('Cycles I created or joined');
+            cy.wrap(inter)
+                .its('response.body')
+                .then(body=>{
+                        console.log(body,'body')
+                        cy.wrap(body).should('have.a.property','data')
+        
+                        cy.get('[data-cy="myCycles"]').within(()=>{
+                            cy.get('[data-cy^="mosaic-item-cycle-"]').should('have.lengthOf',body.data.length)
+                        })
+        
+                    })
 
-            })
         })
+
+        // cy.visit('/en')
+        // const url = `/api/cycle?props=*`
+        // cy.intercept({method:'GET',url}).as('cyclesCreatedOrJoinedReq')
+        
+        // cy.wait('@cyclesCreatedOrJoinedReq',{timeout:30000}).then(inter=>{
+        //     cy.contains('Cycles I created or joined')
+        //     cy.wrap(inter).its('response.body').then(body=>{
+        //         console.log(body,'body')
+        //         cy.wrap(body).should('have.a.property','data')
+
+        //         cy.get('[data-cy="myCycles"]').within(()=>{
+        //             cy.get('[data-cy^="mosaic-item-cycle-"]').should('have.lengthOf',body.data.length)
+        //         })
+
+        //     })
+        // })
     }) 
 })
 
