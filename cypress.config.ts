075903980c58e3332da38382352af70cb037d130
Geordanis B. Vega <gbanoaol@gmail.com>
2022-09-13 12:19:02 -0400

    fe-incressing-drammatically-pageLoadTimeout

--- cypress.config.ts
+++ cypress.config.ts
@@ -23,7 +23,9 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
     baseUrl: 'http://localhost:3000',
+    video:false
   },
+  pageLoadTimeout:1000000,
   requestTimeout: 8000,
   defaultCommandTimeout: 8000,
 });
