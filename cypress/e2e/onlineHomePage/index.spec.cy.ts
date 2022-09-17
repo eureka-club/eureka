7ee31e9c1bf95fd2fa83ad0fd2de333bc15215da
Geordanis B. Vega <gbanoaol@gmail.com>
2022-09-17 10:37:00 -0400

    im-test-timeout

--- cypress/e2e/onlineHomePage/index.spec.cy.ts
+++ cypress/e2e/onlineHomePage/index.spec.cy.ts
@@ -1,7 +1,9 @@
 import { signIn } from "next-auth/react";
 
 describe('Online home page',()=>{
+    const options = {timeout:100000}
     beforeEach(()=>{
+
         // signIn('credentials' ,{
         //     redirect:false,
         //     email:'gbanoaol@gmail.com',
@@ -20,10 +22,34 @@ describe('Online home page',()=>{
     //en/
     it('should login works',()=>{
         cy.visit('/en')  
-        cy.get('[data-cy="login-form"]').find('[type="email"]').type('gbanoaol@gmail.com',{force:true})
-        cy.get('[data-cy="login-form"]').find('[type="password"]').type('gbanoaol@gmail.com1',{force:true})
-        cy.get('[data-cy="login-form"]').find("[data-cy='btn-login']")
-        .click({force:true})
+        cy.get('[data-cy="login-form"]',options).find('[type="email"]').type('gbanoaol@gmail.com',{force:true})
+        cy.get('[data-cy="login-form"]',options).find('[type="password"]').type('gbanoaol@gmail.com1',{force:true})
+        cy.get('[data-cy="login-form"]',options).find("[data-cy='btn-login']")
+            .click({force:true})
+
+
+        
+        // cy.intercept('/api/auth/session').as('sessionReq')
+        // cy.wait('@sessionReq').then(inter=>{
+        //     cy.wrap(inter).its('response').its('body').its('user').then(u=>{
+        //         expect(u.email).to.be.eq('gbanoaol@gmail.com')
+
+        //         cy.contains('Cycles I created or joined',options)
+        //         cy.get('[data-cy="myCycles"]',options)
+
+        //         cy.contains('Trending topics',options)
+        //         cy.get('[data-cy="tag"]',options)
+
+        //         cy.contains('Gender and feminisms',options)
+        //         cy.get('[data-cy="carousel-gender-feminisms"]',options)
+
+        //         // cy.get('footer').scrollIntoView().should('be.visible')
+        //         // cy.contains('Environment',options)
+        //         // cy.get('[data-cy="carousel-environment"]',options)
+
+        //     })
+        // })
+
 
         // cy.wait(10000)
         // signIn('credentials' ,{
@@ -85,70 +111,62 @@ describe('Online home page',()=>{
         
     // })
     
-    // it('should contains main elements',()=>{
-    //     cy.intercept('/api/auth/session').as('sessionReq')
-    //     cy.wait('@sessionReq').then(inter=>{
-    //         cy.wrap(inter).its('response').its('body').its('user').then(u=>{
-    //             expect(u.email).to.be.eq('gbanoaol@gmail.com')
-    //         })
-    //     })
-    // })
     
     it('should have a section "Cycles I created or joined"', ()=>{
         // cy.visit('/en')
 
-        cy.contains('Cycles I created or joined')
-        cy.get('[data-cy="myCycles"]')
+        cy.contains('Cycles I created or joined',options)
+        cy.get('[data-cy="myCycles"]',options)
     })
 
     it('should have in "Cycles I created or joined" the correct cycle mosaics qty', ()=>{
         const url = '/api/cycle?props=%7B%22where%22%3A%7B%22OR%22%3A%5B%7B%22participants%22%3A%7B%22some%22%3A%7B%22id%22%3A127%7D%7D%7D%2C%7B%22creatorId%22%3A127%7D%5D%7D%7D'
         cy.request(url).its('body').should('have.a.property','data').then(data=>{
-            cy.get('[data-cy="myCycles"]').within(()=>{
-                cy.get('[data-cy^="mosaic-item-cycle-"]').should('have.lengthOf',data.length)
+            cy.get('[data-cy="myCycles"]',options).within(()=>{
+                cy.get('[data-cy^="mosaic-item-cycle-"]',options).should('have.lengthOf',data.length)
             })
         })
     })
 
     it('should have a section "Trending topics"', ()=>{
-        cy.contains('Trending topics')
-        cy.get('[data-cy="tag"]')
+        cy.contains('Trending topics',options)
+        cy.get('[data-cy="tag"]',options)
     }) 
 
     it('should have a carousel "Gender and feminisms"',()=>{
-        cy.contains('Gender and feminisms')
-        // cy.get('[data-cy="carousel-gender-feminisms"]')
+        cy.contains('Gender and feminisms',options)
+        cy.get('[data-cy="carousel-gender-feminisms"]',options)
     })
 
     it('should load the carousel "Environment"',()=>{
         cy.scrollTo('bottom')
         cy.scrollTo('bottom')
-        cy.contains('Environment')
-        // cy.get('[data-cy="carousel-environment"]')
+        cy.contains('Environment',options)
+        cy.get('[data-cy="carousel-environment"]',options)
     })
 
     it('should have a "Create" button with link to creat Cycle, Eureka and Work',()=>{
-        cy.contains('button','Create').click({force:true})
-        cy.get('[class="dropdown-menu show"]')
-        cy.get('[class="dropdown-item"]').contains('Cycle').should('have.attr','href','/en/cycle/create')
-        cy.get('[class="dropdown-item"]').contains('Eureka').should('have.attr','href','/en/post/create')
-        cy.get('[class="dropdown-item"]').contains('Work').should('have.attr','href','/en/work/create')
+        cy.contains('button','Create',options).click({force:true})
+        cy.get('[class="dropdown-menu show"]',options)
+        cy.get('[class="dropdown-item"]',options).contains('Cycle').should('have.attr','href','/en/cycle/create')
+        cy.get('[class="dropdown-item"]',options).contains('Eureka').should('have.attr','href','/en/post/create')
+        cy.get('[class="dropdown-item"]',options).contains('Work').should('have.attr','href','/en/work/create')
     })
 
     it('should have "My Mediatheque" link',()=>{
-        cy.contains('My Mediatheque')
+        cy.contains('My Mediatheque',options)
     })
 
     it('should have session button with actions: Edit Profile, Administrator dashboard and Logout',()=>{
-        cy.get('[data-cy="session-actions"]').find('button').click({force:true})
-        cy.contains('Edit Profile')
-        cy.contains('Administrator dashboard')
-        cy.contains('Logout')
+        cy.get('[data-cy="session-actions"]',options).find('button').click({force:true})
+        cy.contains('Edit Profile',options)
+        cy.contains('Administrator dashboard',options)
+        cy.contains('Logout',options)
     })
 
     it('should have the "Notification" section',()=>{
-        cy.get('[data-cy="notifications-btn"]').find('button').click({force:true})
-        cy.get('.NotificationsList')
+        cy.get('[data-cy="notifications-btn"]',options).find('button').click({force:true})
+        cy.get('.NotificationsList',options)
     })
 
     
