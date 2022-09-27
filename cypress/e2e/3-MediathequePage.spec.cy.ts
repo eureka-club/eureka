6fd4564d39de4baf579d9a983616a27f625ffe53
Geordanis B. Vega <gbanoaol@gmail.com>
2022-09-27 14:30:17 -0400

    fe-test-related-to-cycles-created-or-joined

--- cypress/e2e/3-MediathequePage.spec.cy.ts
+++ cypress/e2e/3-MediathequePage.spec.cy.ts
@@ -14,48 +14,38 @@ describe('Mediatheque page',()=>{
 
     })
 
-    it('should have "My Eurekas" section',()=>{
+    it('should have "Cycles I created or joined" section',()=>{
         cy.wait(10000)
         cy.visit('/en/mediatheque/geordanis-vega-127')
-        cy.contains('My Eurekas',options)
 
-        // cy.visit('/en/mediatheque/geordanis-vega-127',{
-        //     // onLoad(win) {
-        //     //     debugger;
-
-        //     //     cy.wrap(win).its('__NEXT_DATA__').its('props').then(props=>{
-        //     //         debugger;
-        //     //         console.log(props)
-        //     //     }) 
-        //     // },
-        // }).then(win=>{
-        //     console.log(win,'win')
-        //     cy.wrap(win).its('__NEXT_DATA__').its('props').its('pageProps').then(pageProps=>{
-        //         console.log(pageProps)
-        //     })
-        // }) 
-
+        let url = '/api/cycle?props=%7B%22where%22%3A%7B%22OR%22%3A%5B%7B%22participants%22%3A%7B%22some%22%3A%7B%22id%22%3A127%7D%7D%7D%2C%7B%22creatorId%22%3A127%7D%5D%7D%7D'
+        cy.request(url).its('body').then(body=>{
+            const {data}= body
+            cy.contains('Cycles I created or joined',options)
+            cy.get('[data-cy^="mosaic-item-cycle-"]').should('have.length',data.length)
+        })
+        
     })
 
-    it('should have "Cycles I created or joined" section',()=>{
-        cy.contains('Cycles I created or joined',options)
-        cy.get('[data-cy="cycles-created-or-joined"]',options)
-    })
+    // it('should have "Cycles I created or joined" section',()=>{
+    //     cy.contains('Cycles I created or joined',options)
+    //     cy.get('[data-cy="cycles-created-or-joined"]',options)
+    // })
 
-    it('should have the "My ratings: movies and books I watched or read" section',()=>{
-        cy.contains('My ratings: movies and books I watched or read',options)
-        cy.get('[data-cy="my-books-movies"]',options)
-    })
+    // it('should have the "My ratings: movies and books I watched or read" section',()=>{
+    //     cy.contains('My ratings: movies and books I watched or read',options)
+    //     cy.get('[data-cy="my-books-movies"]',options)
+    // })
 
-    it('should have the "Saved for later or forever" section',()=>{
-        cy.contains('Saved for later or forever',options)
-        cy.get('[data-cy="my-saved"]',options)
-    })
+    // it('should have the "Saved for later or forever" section',()=>{
+    //     cy.contains('Saved for later or forever',options)
+    //     cy.get('[data-cy="my-saved"]',options)
+    // })
 
-    it('should have the "Users I follow" section',()=>{
-        cy.contains('Saved for later or forever',options)
-        cy.get('[data-cy="my-users-followed"]',options)
-    })
+    // it('should have the "Users I follow" section',()=>{
+    //     cy.contains('Saved for later or forever',options)
+    //     cy.get('[data-cy="my-users-followed"]',options)
+    // })
     
 })
 export {}
