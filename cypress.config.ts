9f70e6192b82b317e382e4bfd05513c28393939a
Geordanis B. Vega <gbanoaol@gmail.com>
2022-10-05 08:00:59 -0400

    im-mediatheuqe-tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
     video:process.env.NODE_ENV=='development',
     chromeWebSecurity: false
   },
-  pageLoadTimeout:60000,
-  requestTimeout: 60000,
-  defaultCommandTimeout: 60000,
+  pageLoadTimeout:30000,
+  requestTimeout: 30000,
+  defaultCommandTimeout: 30000,
 });
