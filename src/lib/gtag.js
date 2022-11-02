c3545903c54859e2916ac4d56504f74ef4d6b61e
Geordanis B. Vega <gbanoaol@gmail.com>
2022-11-02 10:59:22 -0400

    fe-adding-gtm-code-dinamically

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -1,4 +1,6 @@
-export const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID
+export const GTM_ID = process.env.NODE_ENV=='development' 
+  ? process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID_STAGING
+  : process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID
 
 export const pageview = (url) => {
   window.dataLayer.push({
