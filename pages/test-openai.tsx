725ce93b19038ead2813d458b90ffdbb74bd8f62
Geordanis B. Vega <gbanoaol@gmail.com>
2022-11-22 16:16:18 -0500

    fe-openai

---
+++ pages/test-openai.tsx
@@ -0,0 +1,73 @@
+import { GetServerSideProps,NextPage } from 'next';
+import useTranslation from 'next-translate/useTranslation';
+import { useRouter } from 'next/router';
+import {getSession, useSession } from 'next-auth/react';
+import { useState, useEffect, SyntheticEvent, ChangeEvent, MouseEventHandler, FormEvent } from 'react';
+import { Spinner, Card, Row, Col, ButtonGroup, Button, Alert } from 'react-bootstrap';
+import { BiArrowBack } from 'react-icons/bi';
+import SimpleLayout from '../src/components/layouts/SimpleLayout';
+import EditUserForm from '@/components/forms/EditUserForm';
+
+
+interface Props {
+}
+
+const TestOpenai: NextPage<Props> = () => {
+  const [loading,setLoading] = useState(false)
+  const [text,setText] = useState('')
+  const [images,setImages] = useState<HTMLImageElement[]>([])
+
+  function onTextChange(e:ChangeEvent<HTMLTextAreaElement>){
+    setText(e.target.value)
+  }
+  async function onSubmit(form:FormEvent<HTMLFormElement>){
+    setLoading(true)
+    setImages([])
+    form.preventDefault()
+    const {data:{data}} =  await fetch('/api/openai/createImage',{
+      method:'POST',
+      headers:{
+          'Content-type':'application/json'
+      },
+      body:JSON.stringify({text})
+    }).then(r=>r.json())
+
+    const promises = (data as {b64_json:string}[]).map(d=>{
+      return new Promise((resolve,reject)=>{
+        const img = new Image()
+        img.onload = function(){
+          setImages(res=>[...res,img])
+          resolve(true)
+        }
+        img.src = `data:image/webp;base64,${d.b64_json}`
+      })
+    })
+    await Promise.all(promises)
+    setLoading(false)
+  }
+
+  const renderImages = ()=>{
+    return images.map((img,idx)=><img key={idx} src={img.src}/>)
+  }
+
+  return <SimpleLayout title={'test openai'}>
+      <form onSubmit={onSubmit}>
+        <textarea value={text} onChange={onTextChange}></textarea>
+        <button type='submit' disabled={loading}>submit</button>
+      </form>
+      <div>
+        {!loading ? renderImages():<Spinner animation="grow" />}
+      </div>
+    </SimpleLayout>
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = (await getSession(ctx));
+
+  return {
+    props: {},
+  };
+};
+
+
+export default TestOpenai;
