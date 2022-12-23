c32901b5b10f4ca6fdd02bc7dcf8ee22f168086f
Jose Manuel Gallardo <jmgallardo84@gmail.com>
2022-12-23 11:56:41 -0500

    Fixing style stuff

--- src/components/post/NewMosaicItem.tsx
+++ src/components/post/NewMosaicItem.tsx
@@ -180,9 +180,9 @@ const getParentTitle = () => {
           {post && showdetail && (
           <div className={`${styles.postDetail}`}>
                <div  className={`d-flex flex-row fs-6 `}>
-                <Avatar width={27} height={27} userId={post.creator.id} showFullName={false} size="xs" />
-                <span className={` ms-1 me-1 d-flex align-items-center ${styles.detailText}`}>-</span>
-                <span className={`d-flex align-items-center ${styles.detailText}`}>{dayjs(post.createdAt).format(DATE_FORMAT_SHORT)}</span>
+                <Avatar width={27} height={27} userId={post.creator.id} showFullName={false} size= {(!size) ? "xs" :"sm" } />
+                <span className={` ms-1 me-1 d-flex align-items-center ${(!size) ?  styles.detailText : ""}`}>-</span>
+                <span className={`d-flex align-items-center${(!size) ?  styles.detailText : ""}`}>{dayjs(post.createdAt).format(DATE_FORMAT_SHORT)}</span>
                 </div>
              </div>
             )}
