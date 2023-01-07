36cd5a204fc4225fd03293aff4b4ba66f726fa77
Geordanis B. Vega <gbanoaol@gmail.com>
2023-01-07 17:22:27 -0300

    fe-rewrites

--- middleware.tsx
+++ middleware.tsx
@@ -2,18 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
   const host = req.headers.get('host');
-  if(process.env.NODE_ENV!='development'){
-    if(host?.match('eureka-staging.azurewebsites.net')){
-      return NextResponse.redirect(`https://eureka-staging.azurewebsites.net`, 301);
-    }
-    else{
-      if (host?.match(/^www\..*/i)) {
-        return NextResponse.next();
-      }
-      else{
-        return NextResponse.redirect(`https://www.${host}${req.nextUrl.pathname}`, 301);
-      }
-    }
+  if(host?.match('https://www.eureka-staging.azurewebsites.net')){
+    return NextResponse.redirect(`https://eureka-staging.azurewebsites.net`, 301);
   }
   return NextResponse.next();
 }
