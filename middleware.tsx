be8e3b4eefc8de0575e8c9b0c2ba21ad44b1492e
Geordanis B. Vega <gbanoaol@gmail.com>
2023-01-18 10:34:50 -0300

    hf-adding-rewrite

--- middleware.tsx
+++ middleware.tsx
@@ -2,6 +2,9 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
   const host = req.headers.get('host');
+  console.log('ver headers',JSON.stringify(req.headers))
+
+  console.log('ver host',req.headers.get('host'))
   if(host?.match('https://eureka.club')){
     return NextResponse.redirect(`https://www.eureka.club`, 301);
   }
