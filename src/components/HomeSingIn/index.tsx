7e084f43db7ca0063c09898aa5b1fe7f381b76ca
Jose Manuel Gallardo <jmgallardo84@gmail.com>
2023-03-03 11:32:37 -0500

    Improving my-read-or watch page

--- src/components/HomeSingIn/index.tsx
+++ src/components/HomeSingIn/index.tsx
@@ -2,7 +2,7 @@
 import { FunctionComponent, useEffect, useState, lazy, FC, memo } from 'react';
 import Link from 'next/link';
 import useTranslation from 'next-translate/useTranslation';
-import {Spinner, Col} from 'react-bootstrap';
+import { Spinner, Col } from 'react-bootstrap';
 import TagsInput from '@/components/forms/controls/TagsInput';
 import { GetAllByResonse } from '@/src/types';
 import { useInView } from 'react-intersection-observer';
@@ -11,7 +11,7 @@ import { UserMosaicItem } from '@/src/types/user';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import slugify from 'slugify'
-import useMyCycles,{myCyclesWhere} from '@/src/useMyCycles';
+import useMyCycles, { myCyclesWhere } from '@/src/useMyCycles';
 import Prompt from '@/src/components/post/PostPrompt';
 import FeaturedCycles from './FeaturedCycles';
 import FeaturedEurekas from './FeaturedEurekas';
@@ -19,28 +19,30 @@ import FeaturedWorks from '@/src/components/HomeSingIn/FeaturedWorks';
 import CarouselsByTopics from './CarouselsByTopics';
 import CarouselStatic from '../CarouselStatic';
 import FeaturedUsers from './FeaturedUsers';
+import { BsChevronUp, BsChevronDown } from 'react-icons/bs';
+
 
 const topics = ['gender-feminisms', 'technology', 'environment',
- 'racism-discrimination',
-    'wellness-sports','social issues',
-    'politics-economics','philosophy',
-    'migrants-refugees','introspection',
-    'sciences','arts-culture','history',
+  'racism-discrimination',
+  'wellness-sports', 'social issues',
+  'politics-economics', 'philosophy',
+  'migrants-refugees', 'introspection',
+  'sciences', 'arts-culture', 'history',
 ];
 
-const fetchItems = async (pageParam: number,topic:string,sessionId:number):Promise<GetAllByResonse> => {
-        const url = `${process.env.NEXT_PUBLIC_WEBAPP_URL}/api/getAllBy?topic=${topic}&cursor=${pageParam}&sessionId=${sessionId}`;
-        const q = await fetch(url);
-        return q.json();
+const fetchItems = async (pageParam: number, topic: string, sessionId: number): Promise<GetAllByResonse> => {
+  const url = `${process.env.NEXT_PUBLIC_WEBAPP_URL}/api/getAllBy?topic=${topic}&cursor=${pageParam}&sessionId=${sessionId}`;
+  const q = await fetch(url);
+  return q.json();
 };
 
-interface Props{
-        groupedByTopics: Record<string,GetAllByResonse>;
-        // myCycles?:CycleMosaicItem[]
-      }
-      
-const HomeSingIn: FunctionComponent<Props> = ({ groupedByTopics}) => {
-  const {data:session, status} = useSession();
+interface Props {
+  groupedByTopics: Record<string, GetAllByResonse>;
+  // myCycles?:CycleMosaicItem[]
+}
+
+const HomeSingIn: FunctionComponent<Props> = ({ groupedByTopics }) => {
+  const { data: session, status } = useSession();
   const router = useRouter();
   const { t } = useTranslation('common');
   const [ref, inView] = useInView({
@@ -48,84 +50,85 @@ const HomeSingIn: FunctionComponent<Props> = ({ groupedByTopics}) => {
     // rootMargin: '200px 0px',
     // skip: supportsLazyLoading !== false,
   });
-  const [users,setUsers] = useState<UserMosaicItem[]>()
-  const {data:dataCycles} = useMyCycles(session?.user.id!)
-  const [cycles,setCycles] = useState<CycleMosaicItem[]>()
+  const [users, setUsers] = useState<UserMosaicItem[]>()
+  const { data: dataCycles } = useMyCycles(session?.user.id!)
+  const [cycles, setCycles] = useState<CycleMosaicItem[]>()
 
-  useEffect(()=>{
-    if(dataCycles)setCycles(dataCycles.cycles)   
+  useEffect(() => {
+    if (dataCycles) setCycles(dataCycles.cycles)
 
-  },[dataCycles])
+  }, [dataCycles])
 
-  const [gbt, setGBT] = useState([...Object.entries(groupedByTopics||[])]);
-  const [topicIdx,setTopicIdx] = useState(gbt.length-1)
-  
-  useEffect(()=>{
-    const idx = topicIdx+1;
-    if(inView && idx < topics.length){
+  const [gbt, setGBT] = useState([...Object.entries(groupedByTopics || [])]);
+  const [topicIdx, setTopicIdx] = useState(gbt.length - 1)
+  const [showAboutSection, setShowAboutSection] = useState<boolean>(false)
+
+  useEffect(() => {
+    const idx = topicIdx + 1;
+    if (inView && idx < topics.length) {
       let isCanceled = false;
-      if(!isCanceled){
+      if (!isCanceled) {
         const exist = topics[idx] in gbt;
-        const fi = async ()=>{
-          const r = await fetchItems(0,topics[idx],session?.user.id!);
-          gbt.push([topics[idx],r])
-          if(r){
+        const fi = async () => {
+          const r = await fetchItems(0, topics[idx], session?.user.id!);
+          gbt.push([topics[idx], r])
+          if (r) {
             setGBT([...gbt]);
             setTopicIdx(idx);
           }
         }
-        if(!exist)
+        if (!exist)
           fi()
       }
-      return ()=>{
+      return () => {
         isCanceled = true
       }
     }
-  },[inView, gbt, topicIdx]);  
-   
-const getTopicsBadgedLinks = () => {
-        return <TagsInput className='d-flex flex-wrap' formatValue={(v: string) => t(`topics:${v}`)} tags={[...topics].join()} readOnly />;
-      };
+  }, [inView, gbt, topicIdx]);
 
-const renderSpinnerForLoadNextCarousel = ()=>{
-if(topicIdx < topics.length-1) return <Spinner ref={ref} animation="grow" />
-        return '';
-}
+  const getTopicsBadgedLinks = () => {
+    return <TagsInput className='d-flex flex-wrap' formatValue={(v: string) => t(`topics:${v}`)} tags={[...topics].join()} readOnly />;
+  };
 
-const getMediathequeSlug = (id:number,name:string)=>{
-    const s =`${name}`
-    const slug = `${slugify(s,{lower:true})}-${id}` 
+  const renderSpinnerForLoadNextCarousel = () => {
+    if (topicIdx < topics.length - 1) return <Spinner ref={ref} animation="grow" />
+    return '';
+  }
+
+  const getMediathequeSlug = (id: number, name: string) => {
+    const s = `${name}`
+    const slug = `${slugify(s, { lower: true })}-${id}`
     return slug
-}
+  }
 
   const seeAll = async (data: CycleMosaicItem[], q: string, showFilterEngine = true): Promise<void> => {
-    if(session){
+    if (session) {
       const u = session.user
-      router.push(`/user/${getMediathequeSlug(u.id,u.name||u.id.toString())}/my-cycles`);
+      router.push(`/user/${getMediathequeSlug(u.id, u.name || u.id.toString())}/my-cycles`);
     }
   };
-//       <h1 className="text-secondary fw-bold">{t('myCycles')}</h1>
+  //       <h1 className="text-secondary fw-bold">{t('myCycles')}</h1>
 
 
 
   const cyclesJoined = () => {
-  if(!session)return <></>
-  const k = JSON.stringify(myCyclesWhere(session?.user.id))
+    if (!session) return <></>
+    const k = JSON.stringify(myCyclesWhere(session?.user.id))
 
-    return (cycles && cycles.length) 
-    ? <div data-cy="myCycles">
-       <CarouselStatic
-        cacheKey={['CYCLES',k]}
-        onSeeAll={async () => seeAll(cycles, t('myCycles'))}
-        title={t('myCycles')}
-        data={cycles}
-        iconBefore={<></>}
+    return (cycles && cycles.length)
+      ? <div data-cy="myCycles">
+        <CarouselStatic
+          cacheKey={['CYCLES', k]}
+          onSeeAll={async () => seeAll(cycles, t('myCycles'))}
+          title={t('myCycles')}
+          data={cycles}
+          iconBefore={<></>}
         // iconAfter={<BsCircleFill className={styles.infoCircle} />}
-      />
+        />
       </div>
-    : <></>;
+      : <></>;
   };
-   
+
 
   return (
     <>
@@ -139,37 +142,78 @@ const getMediathequeSlug = (id:number,name:string)=>{
           <h2 className="text-secondary fw-bold">{t('Trending topics')}</h2>
           <aside className="mb-4">{getTopicsBadgedLinks()}</aside>
           <section className="mt-5">
-            <h1 className="text-secondary  fw-bold" style={{ fontSize: '1.25rem' }}>
-              {t('About Eureka')}{' '}
+            <h1 className="d-flex flex-row align-items-center text-secondary  fw-bold" style={{ fontSize: '1.25rem' }}>
+              <span>{t('About Eureka')}{' '}</span>
+              {!showAboutSection && (
+                <span
+                  className={`cursor-pointer d-flex d-lg-none ms-4`}
+                  role="presentation"
+                  onClick={() => setShowAboutSection(true)}
+                >
+                  <BsChevronDown style={{ color: 'var(--color-secondary)' }} />
+                </span>
+              )}
+              {showAboutSection && (
+                <span
+                  className={`cursor-pointer d-flex d-lg-none ms-4`}
+                  role="presentation"
+                  onClick={() => setShowAboutSection(false)}
+                >
+                  <BsChevronUp style={{ color: 'var(--color-secondary)' }} />
+                </span>
+              )}
             </h1>
+          </section>
+          {showAboutSection && (<div className='d-flex d-lg-none flex-column'>
             <Link href="/about">
               <a className="text-primary text-decoration-underline text-blue" onClick={() => window.scrollTo(0, 0)}>
                 {t('browserTitleAbout')}{' '}
               </a>
             </Link>
-          </section>
-          <section className="mt-4 p-3 rounded overflow-auto bg-secondary text-white" role="presentation">
-            <h4 className="p-2 m-0 text-wrap text-center fs-6">{t('aboutBox1')}</h4>
-          </section>
-          <section className="mt-5 p-3 rounded overflow-auto bg-yellow text-secondary" role="presentation">
-            <h4 className="p-2 m-0 text-wrap text-center fs-6">{t('aboutBox2')}</h4>
-          </section>
-          <section className="mt-5 p-3 rounded overflow-auto bg-secondary text-white" role="presentation">
-            <h4 className="p-2 m-0 text-wrap text-center fs-6">{t('aboutBox3')}</h4>
-          </section>
-          <section className="mt-5 p-3 rounded overflow-auto bg-yellow text-secondary" role="presentation">
-            <h4 className="p-2 m-0 text-wrap text-center fs-6">{t('aboutBox4')}</h4>
-          </section>
+            <section className="mt-3 p-3 rounded overflow-auto bg-secondary text-white" role="presentation">
+              <h4 className="p-2 m-0 text-wrap text-center fs-6">{t('aboutBox1')}</h4>
+            </section>
+            <section className="mt-3 p-3 rounded overflow-auto bg-yellow text-secondary" role="presentation">
+              <h4 className="p-2 m-0 text-wrap text-center fs-6">{t('aboutBox2')}</h4>
+            </section>
+            <section className="mt-3 p-3 rounded overflow-auto bg-secondary text-white" role="presentation">
+              <h4 className="p-2 m-0 text-wrap text-center fs-6">{t('aboutBox3')}</h4>
+            </section>
+            <section className="mt-3 p-3 mb-3 rounded overflow-auto bg-yellow text-secondary" role="presentation">
+              <h4 className="p-2 m-0 text-wrap text-center fs-6">{t('aboutBox4')}</h4>
+            </section>
+          </div>)}
+          <div className='d-none d-lg-flex flex-column'>
+            <Link href="/about">
+              <a className="text-primary text-decoration-underline text-blue" onClick={() => window.scrollTo(0, 0)}>
+                {t('browserTitleAbout')}{' '}
+              </a>
+            </Link>
+            <section className="mt-4 p-3 rounded overflow-auto bg-secondary text-white" role="presentation">
+              <h4 className="p-2 m-0 text-wrap text-center fs-6">{t('aboutBox1')}</h4>
+            </section>
+            <section className="mt-5 p-3 rounded overflow-auto bg-yellow text-secondary" role="presentation">
+              <h4 className="p-2 m-0 text-wrap text-center fs-6">{t('aboutBox2')}</h4>
+            </section>
+            <section className="mt-5 p-3 rounded overflow-auto bg-secondary text-white" role="presentation">
+              <h4 className="p-2 m-0 text-wrap text-center fs-6">{t('aboutBox3')}</h4>
+            </section>
+            <section className="mt-5 p-3 rounded overflow-auto bg-yellow text-secondary" role="presentation">
+              <h4 className="p-2 m-0 text-wrap text-center fs-6">{t('aboutBox4')}</h4>
+            </section>
+          </div>
+
           <section className="mt-4">
             <FeaturedUsers />
           </section>
+
         </Col>
         <Col xs={12} lg={10} className="mt-5 mt-lg-0">
           <section className="ms-0 ms-lg-5">
             <FeaturedWorks />
             <FeaturedEurekas />
             <FeaturedCycles />
-           {/*cyclesJoined()*/}
+            {/*cyclesJoined()*/}
             <>
               <div className="mt-5">
                 <CarouselsByTopics groupedByTopics={gbt} />
