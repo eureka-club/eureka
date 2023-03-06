82036cf000a8b6bbfaa23356965a8b2790183cb0
Jose Manuel Gallardo <jmgallardo84@gmail.com>
2023-03-06 14:30:52 -0500

    Fixing elements on my read or watched page

--- src/useMyReadOrWatched.tsx
+++ src/useMyReadOrWatched.tsx
@@ -1,16 +1,20 @@
 import { UserMosaicItem } from './types/user';
 import useUser from './useUser';
 
-type Item = UserMosaicItem 
+type Item = UserMosaicItem
 
-const useMyReadOrWatched = (id:number) => {
-  const {data:user} = useUser(id||0,{enabled:!!id});
-  if(!user)return {
-    userName:null,
-    readOrWatchedWorks:[],
+const useMyReadOrWatched = (id: number) => {
+  const { data: user } = useUser(id || 0, { enabled: !!id });
+  if (!user) return {
+    userName: null,
+    image:null,
+    photos:null,
+    readOrWatchedWorks: [],
   }
-  const row={
-   userName:user.name,
+  const row = {
+    userName: user.name,
+    image: user.image,
+    photos: user.photos,
     readOrWatchedWorks: user.readOrWatchedWorks,
   };
   return row;
