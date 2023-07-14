66d51bc533037dbe38d17c72806ef508f689fb99
Geordanis B. Vega <gbanoaol@gmail.com>
2023-07-14 16:41:21 -0300

    bf-off-redirect

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,8 @@ export function middleware(req: NextRequest) {
   const host = req.headers.get('host');
   //console.log('ver host',req.headers.get('host'))
 
-  // if(host?.match('^eureka.club')){
-  //   return NextResponse.redirect(`https://www.eureka.club${req.nextUrl.pathname}`, 301);
-  // }
+  if(host?.match('^eureka.club')){
+    return NextResponse.redirect(`https://www.eureka.club${req.nextUrl.pathname}`, 301);
+  }
   return NextResponse.next();
 }
