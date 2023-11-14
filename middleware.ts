f80a078764d0326be857bf11aa03c62246e1d11d
Geordanis B. Vega <gbanoaol@gmail.com>
2023-11-14 09:54:17 -0300

    fe-added stripe checkout for testing

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export function middleware(req: NextRequest) {
   const host = req.headers.get('host');
   //console.log('ver host',req.headers.get('host'))
-debugger;
+
   if(host?.match('^eureka.club')){
     return NextResponse.redirect(`https://www.eureka.club${req.nextUrl.pathname}`, 301);
   }
