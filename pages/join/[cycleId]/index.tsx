cedc8d2183a76e0409b0f890f329af4abacafbe6
Jose Manuel Gallardo <jmgallardo84@gmail.com>
2023-12-18 15:25:13 -0500

    fe-simple-join-registter-page-to-embbed-wordpress

---
+++ pages/join/[cycleId]/index.tsx
@@ -0,0 +1,28 @@
+import { NextPage, GetServerSideProps } from 'next';
+import useTranslation from 'next-translate/useTranslation';
+import SimpleLayout from '@/src/components/layouts/SimpleLayout';
+import SignUpJoinToCycleSimpleForm from '@/src/components/forms/SignUpJoinToCycleSimpleForm';
+import { getSession } from 'next-auth/react';
+
+// interface Props {
+//   session: any;
+// }
+
+const RegisterJoinToCyclePage: NextPage = ({  }) => {
+  const { t } = useTranslation('signUpForm'); 
+  
+  return (
+    <SimpleLayout title={t('Sign up')} showNavBar={false} showFooter={false}>
+      <SignUpJoinToCycleSimpleForm noModal/>
+    </SimpleLayout>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getSession(ctx);
+  return {
+    props: { session },
+  };
+};
+
+export default RegisterJoinToCyclePage;
