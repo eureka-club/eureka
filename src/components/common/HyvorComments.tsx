7214a2aa167d7a05b20f74f3430e5cf6f472208f
Geordanis B. Vega <gbanoaol@gmail.com>
2023-12-29 07:48:57 -0300

    bf-hyvortalk-error-with-msg-delivery

--- src/components/common/HyvorComments.tsx
+++ src/components/common/HyvorComments.tsx
@@ -1,6 +1,7 @@
 import {memo,FC} from 'react';
 import crypto from 'crypto-js';
 import { Embed, CommentCount } from 'hyvor-talk-react';
+import { Comments } from '@hyvor/hyvor-talk-react';
 //import { useSession } from 'next-auth/react';
 
 import { HYVOR_SSO_KEY, HYVOR_WEBSITE_ID, WEBAPP_URL } from '@/src/constants';
@@ -15,6 +16,8 @@ const HyvorComments:FC<Props> = ({ entity,id,session })=>{
   //const {data:session, status} = useSession() ;
   const isSessionLoading = status == 'loading'
   let hyvorSso = {};
+  let hash;
+  let userData;
 const { NEXT_PUBLIC_AZURE_CDN_ENDPOINT } = process.env;
 const { NEXT_PUBLIC_AZURE_STORAGE_ACCOUNT_CONTAINER_NAME } = process.env;
 
@@ -23,26 +26,37 @@ const { NEXT_PUBLIC_AZURE_STORAGE_ACCOUNT_CONTAINER_NAME } = process.env;
   }
 
   if (session == null) {
-    const userData = Buffer.from(JSON.stringify({})).toString('base64');
-    const hash = crypto.HmacSHA1(userData, HYVOR_SSO_KEY!).toString();
-    hyvorSso = { hash, userData, loginURL: `${WEBAPP_URL}/` };
-  } else {
+    // userData = Buffer.from(JSON.stringify({
+    //   timestamp: Math.floor(Date.now() / 1000),
+    // })).toString('base64');
+    // hash = crypto.HmacSHA1(userData, HYVOR_SSO_KEY!).toString();
+    // hyvorSso = { hash, userData, loginURL: `${WEBAPP_URL}/` };
+  } 
+  if(session) {
     const { user } = session;
     const userDataObj = {
       id: user.id,
-      email: user.email,
       name: user.name || user.email?.split('@')[0] || 'User',
-      picture: (user.photos && user.photos.length) 
+      email: user.email,
+      picture_url: (user.photos && user.photos.length) 
       ? `https://${NEXT_PUBLIC_AZURE_CDN_ENDPOINT}.azureedge.net/${NEXT_PUBLIC_AZURE_STORAGE_ACCOUNT_CONTAINER_NAME}/users-photos/${user.photos[0].storedFile}` 
       : user.image,
+      timestamp: Math.floor(Date.now() / 1000),
     };
-    const userData = Buffer.from(JSON.stringify(userDataObj)).toString('base64');
-    const hash = crypto.HmacSHA1(userData, HYVOR_SSO_KEY!).toString();
+    debugger;
+    userData = Buffer.from(JSON.stringify(userDataObj)).toString('base64');
+    hash = crypto.HmacSHA256(userData, HYVOR_SSO_KEY!).toString();
 
     hyvorSso = { hash, userData, loginURL: `${WEBAPP_URL}/` };
+    return <Comments
+              website-id={3377}
+              page-id={`${entity}-${id}`}
+              sso-user={userData}
+              sso-hash={hash}
+          />
   }
-
-  return <Embed websiteId={Number(3377)} id={`${entity}-${id}`} sso={hyvorSso}/>;
+  return <></>
+  // return <Embed websiteId={Number(3377)} id={`${entity}-${id}`} sso={hyvorSso}/>;
 };
 
 export default memo(HyvorComments);
