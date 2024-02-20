796be2adbdc45908ecd2a08bf239a5939b3951b9
Geordanis B. Vega <gbanoaol@gmail.com>
2024-02-20 20:24:47 -0300

    fe-test-redis-install-on-same-docker-undo

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,11 +1,11 @@
 import Redis from 'ioredis';
 
-// const url =
-//   process.env.NODE_ENV === 'development'
-//     ? process.env.UPSTASH_REDIS_REST_URL_STAGING
-//     : process.env.UPSTASH_REDIS_REST_URL;
-// const client = new Redis(url!);
-const client = process.env.NODE_ENV == 'development' 
-  ? new Redis()
-  : new Redis(6379, "20.119.144.9");
+const url =
+  process.env.NODE_ENV === 'development'
+    ? process.env.UPSTASH_REDIS_REST_URL_STAGING
+    : process.env.UPSTASH_REDIS_REST_URL;
+const client = new Redis(url!);
+// const client = process.env.NODE_ENV == 'development' 
+//   ? new Redis()
+//   : new Redis(6379, "20.119.144.9");
 export default client;
