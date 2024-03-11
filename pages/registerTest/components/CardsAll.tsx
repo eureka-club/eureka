24d4c4530e6f949a90f1cbb50c5d027bfcc5e7a1
Geordanis B. Vega <gbanoaol@gmail.com>
2024-03-11 17:53:34 -0300

    im-cards-all-display

--- pages/registerTest/components/CardsAll.tsx
+++ pages/registerTest/components/CardsAll.tsx
@@ -11,34 +11,34 @@ import Stars from './Stars';
 export default function CardsAll() {
   return (
     <>
-      <Stack direction={{ xs: 'column', sm: 'row' }} gap={2}>
+      <Stack id="asUl" direction={{ xs: 'column'}} gap={2}>
         
-        <Stack id="asUl" gap={5} justifyContent={'space-between'}>
-        <style jsx global>{
-              `
-                  #asUl{
-                      padding:0;
-                      counter-reset:my-counter;
-                  }
-                  #asUl .MuiGrid-item{
-                      list-style:none;
-                  }
-                  #asUl .MuiGrid-item em:before{
-                      counter-increment: my-counter;
-                      padding:.5rem 1.2rem;
-                      margin-right:.5rem;
-                      content:counter(my-counter) ;
-                      border:solid 1px var(--color-secondary);
-                      background:var(--color-secondary);
-                      color:white;
-                      font-size:2rem;
-                      border-radius:100%;
-                  }
-              `
-              }
+          <style jsx global>{
+                `
+                    #asUl{
+                        padding:0;
+                        counter-reset:my-counter;
+                    }
+                    #asUl .MuiGrid-item{
+                        list-style:none;
+                    }
+                    #asUl .MuiGrid-item em:before{
+                        counter-increment: my-counter;
+                        padding:.5rem 1.2rem;
+                        margin-right:.5rem;
+                        content:counter(my-counter) ;
+                        border:solid 1px var(--color-secondary);
+                        background:var(--color-secondary);
+                        color:white;
+                        font-size:2rem;
+                        border-radius:100%;
+                    }
+                `
+                }
           </style>
-          <Grid item xs={12} sm={6}>
-            <Box border={0} >
+
+          <Grid container>
+            <Grid item xs={12} sm={6} padding={2}>
               <Card elevation={0} sx={{
                 maxWidth: 600, transition: "0.2s",
                 "&:hover": {
@@ -77,11 +77,55 @@ export default function CardsAll() {
                 </CardContent>
 
               </Card>
-            </Box>
+            </Grid>
+            <Grid item xs={12} sm={6} padding={2}>
+              <Card elevation={5} sx={{
+                maxWidth: 500, transition: "0.2s",
+                "&:hover": {
+                  transform: "scale(1.05)",
+
+                },
+
+                justifyContent: "left",
+                alignItems: "left",
+                
+              }}>
+                <CardHeader justifyContent="left"
+
+                  avatar={
+                    <Avatar src='/img/Anna.webp' aria-label="recipe" >
+
+                    </Avatar>
+                  }
+
+                  action={
+                    <IconButton aria-label="settings">
+
+                      <Stars />
+                    </IconButton>
+                  }
+
+                  title="Anna Silva"
+                  subheader="Marketing, Brasil"
+                />
+
+                <Divider />
+                <CardContent>
+
+                  <Typography textAlign={"justify"} variant="body2" color="text.secondary">
+                    “Eureka Club me proporcionó un viaje increíble de aprendizaje y reflexión
+                    sobre la justicia social. Participar en el club me hizo replantear mi visión del mundo.
+                    Lo recomiendo a todos los que deseen expandir sus horizontes y actuar en pro de la justicia.”
+
+                  </Typography>
+                </CardContent>
+
+              </Card>
+            </Grid>
           </Grid>
 
-          <Grid item xs={12} sm={6}>
-            <Box border={0}>
+          <Grid container>
+            <Grid item xs={12} sm={6} padding={2}>
               <Card elevation={0} sx={{
                 maxWidth: 600, transition: "0.2s",
                 "&:hover": {
@@ -96,7 +140,6 @@ export default function CardsAll() {
                                                
                                                borderRight: { xs: 'none', sm: 'solid .2rem ', md: 'solid .2rem ', color:"#504788" }
                                            }}>
-                      {/* <img src="2.webp" alt="" width={80} height={80} /> */}
                       <em></em>
 
                     </Box>
@@ -119,11 +162,55 @@ export default function CardsAll() {
                 </CardContent>
 
               </Card>
-            </Box>
+            </Grid>
+            <Grid item xs={12} sm={6} padding={2}>
+              <Card elevation={5} sx={{
+                maxWidth: 500, transition: "0.2s",
+                "&:hover": {
+                  transform: "scale(1.05)",
+
+                },
+
+                justifyContent: "left",
+                alignItems: "left",
+                
+              }}>
+                <CardHeader justifyContent="left"
+
+                  avatar={
+                    <Avatar src='/img/Andreia.webp' aria-label="recipe" >
+
+                    </Avatar>
+                  }
+
+                  action={
+                    <IconButton aria-label="settings">
+
+                      <Stars/>
+                    </IconButton>
+                  }
+
+                  title="Andreia Odriozola"
+                  subheader="Periodista, Uruguay"
+                />
+
+                <Divider />
+                <CardContent>
+
+                  <Typography textAlign={"justify"} variant="body2" color="text.secondary">
+                    “Participar en el club fue una de las mejores decisiones que he tomado.
+                    Los temas me ayudaron a comprender cómo podemos construir un mundo más justo y sostenible.
+                    Cada libro recomendado inspiró cambios en mi vida personal y profesional.”
+
+                  </Typography>
+                </CardContent>
+
+              </Card>
+            </Grid>
           </Grid>
 
-          <Grid item xs={12} sm={6}>
-            <Box border={0}>
+          <Grid container>
+            <Grid item xs={12} sm={6} padding={2}>
               <Card elevation={0} sx={{
                 maxWidth: 600, transition: "0.2s",
                 "&:hover": {
@@ -138,7 +225,6 @@ export default function CardsAll() {
                                                
                                                borderRight: { xs: 'none', sm: 'solid .2rem ', md: 'solid .2rem ', color:"#504788" }
                                            }}>
-                      {/* <img src="3.webp" alt="" width={80} height={80} /> */}
                       <em></em>
 
                     </Box>
@@ -161,12 +247,54 @@ export default function CardsAll() {
                 </CardContent>
 
               </Card>
-            </Box>
+            </Grid>
+            <Grid item xs={12} sm={6} padding={2}>
+              <Card elevation={5} sx={{
+                maxWidth: 500, transition: "0.2s",
+                "&:hover": {
+                  transform: "scale(1.05)",
+
+                },
+
+                justifyContent: "left",
+                alignItems: "left",
+                
+              }}>
+                <CardHeader justifyContent="left"
+
+                  avatar={
+                    <Avatar src='/img/David.webp' aria-label="recipe" >
+
+                    </Avatar>
+                  }
+
+                  action={
+                    <IconButton aria-label="settings">
+
+                      <Stars />
+                    </IconButton>
+                  }
+
+                  title="David Barbosa"
+                  subheader="Empresario, Brasil"
+                />
+
+                <Divider />
+                <CardContent>
+
+                  <Typography textAlign={"justify"} variant="body2" color="text.secondary">
+                    “¡Eureka Club es una comunidad increíble! Tenía algunas dudas sobre cómo
+                    un hombre podría encajar en un espacio enfocado en el feminismo, pero pronto
+                    me di cuenta de que era un lugar de aprendizaje y crecimiento para todos.”
+
+                  </Typography>
+                </CardContent>
+
+              </Card>
+            </Grid>
           </Grid>
 
-        </Stack>
-
-        <Stack gap={5} justifyContent={'space-between'}>
+        {/* <Stack gap={5} justifyContent={'space-between'}>
 
           <Grid item xs={12} sm={6} paddingRight={1} paddingLeft={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
             <Box maxWidth={500}  >
@@ -309,7 +437,7 @@ export default function CardsAll() {
             </Box>
           </Grid>
 
-        </Stack>
+        </Stack> */}
 
       </Stack>
     </>
