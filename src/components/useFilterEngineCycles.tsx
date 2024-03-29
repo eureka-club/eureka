aaad1f885d1abdfda57faa37246a038bd43d634f
Geordanis B. Vega <gbanoaol@gmail.com>
2024-03-29 09:16:33 -0300

    hf-wrong-display-on-mobile

--- src/components/useFilterEngineCycles.tsx
+++ src/components/useFilterEngineCycles.tsx
@@ -1,6 +1,6 @@
 import React, { useState,ChangeEvent  } from "react"
 import useTranslation from 'next-translate/useTranslation';
-import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
+import { Checkbox, FormControlLabel, FormGroup, Stack } from "@mui/material";
 import useAutocompleteRegions from "./useAutocompleteRegions";
 export interface FiltersType {
   public:boolean;
@@ -21,22 +21,17 @@ const useFilterEngineCycles = ()=>{
     
     const FilterEngineCycles: React.FC = ()=>{
       
-    return <section className="d-flex flex-row align-items-center justify-content-end my-2">
-    <div className="my-3">
-      <FormGroup row>
+    return <Stack paddingTop={2} paddingBottom={2} justifyContent={'center'} alignItems={'center'}>
+      <FormGroup row sx={{justifyContent:'center'}}>
         <FormControlLabel label={t('private')}control={
           <Checkbox data-cy="check-private" checked={filtersType['private']} onChange={(e) => handlerComboxesChangeType(e, 'private')}/>
         }/>
         <FormControlLabel label={t('public')} control={
           <Checkbox data-cy="check-public" checked={filtersType['public']} onChange={(e) => handlerComboxesChangeType(e, 'public')}/>
         }/>
-        <FormControlLabel label='' control={
-          <AutocompleteRegions/>
-        }
-      />
       </FormGroup>
-    </div>
-    </section>
+      <AutocompleteRegions/>
+    </Stack>
     }
     return {FilterEngineCycles,filtersType,filtersCountries};
 }
