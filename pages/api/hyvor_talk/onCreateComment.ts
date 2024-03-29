886e90c43b3346e6457c57a74f68539cedce93e5
Geordanis B. Vega <gbanoaol@gmail.com>
2024-03-29 17:05:31 -0300

    fe-hyvor-talk-webhook

---
+++ pages/api/hyvor_talk/onCreateComment.ts
@@ -0,0 +1,80 @@
+import { sendMail } from '@/src/facades/mail';
+import { create } from '@/src/facades/notification';
+import Notifier from '@/src/lib/Notifier';
+import { compare, compareSync } from 'bcryptjs';
+import { createHash, createHmac } from 'crypto';
+import type { NextApiRequest, NextApiResponse } from 'next'
+const secretKey = process.env.HYVOR_TALK_Webhook_Secret;
+
+const buffer = (req:any) => {
+    return new Promise((resolve, reject) => {
+      const chunks: any[] = [];
+  
+      req.on('data', (chunk: any) => {
+        chunks.push(chunk);
+      });
+  
+      req.on('end', () => {
+        resolve(Buffer.concat(chunks));
+      });
+  
+      req.on('error', reject);
+    });
+  };
+
+export const config = {
+    api: {
+      bodyParser: false,
+    },
+  };
+
+type Data = {
+  data?: Object;
+  error?:string;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+    if(req.method=='POST'){
+        try{
+          const bodyBuffer = await buffer(req);
+          debugger;
+          const givenSignature = req.headers['HTTP_X_SIGNATURE']??req.headers['http_x_signature'];
+          if(givenSignature){
+            const body = JSON.parse(bodyBuffer!.toString()); 
+            const {
+                event,
+                data
+            } = body;
+
+            const userId = data?.user?.sso_id??undefined;
+            const name = data?.user?.name??undefined;
+            // const body_html = data?.body_html??'';
+            const title = data?.page?.title??'';
+            // const identifier = data?.page?.identifier??'';
+            const url = data?.page?.url??'';
+          
+            await sendMail({
+                  from:{email:process.env.EMAILING_FROM!},
+                  to:[
+                    {email:process.env.DEV_EMAIL!},
+                    {email:process.env.EMAILING_FROM!}
+                  ],
+                  subject:`gbanoaol@gmail.com`,
+                  html:`${givenSignature}`
+            });
+            
+            let sense = (event??'').replace(/^\w+\.(\w+)/g,'$1');
+            const msg=`hyvor-talk-comment-${sense}!|!{"userName":"${name}","cycleTitle":"${title}"}`;
+            await create(msg,url,127,[2,127]);
+          
+            return res.status(200).json({ data:{data,event} });
+          }
+        }
+        catch(e){
+          return res.status(400).json({ error:'Server Error' });
+        }
+    }
+}
