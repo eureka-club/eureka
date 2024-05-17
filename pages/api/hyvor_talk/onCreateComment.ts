5ce95976c1e6068cc37c022ce579d1401aec1229
Geordanis B. Vega <gbanoaol@gmail.com>
2024-05-17 13:10:58 -0300

    bf-fixing-wrong-scheduled

--- pages/api/hyvor_talk/onCreateComment.ts
+++ pages/api/hyvor_talk/onCreateComment.ts
@@ -183,7 +183,7 @@ export default async function handler(
             }
           });
           if(comentEmailSaved){
-            const cronTime = '* * 20 * * *';
+            const cronTime = '0 0 20 * * *';
             if(!(global as any).job){
               const dt = sendAt(cronTime);
               console.log(`The job would run at: ${dt.toISO()}`);
