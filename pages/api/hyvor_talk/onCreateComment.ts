7c2e53fe1990bd4d2927c6f647d3a4b51139ecb3
Geordanis B. Vega <gbanoaol@gmail.com>
2024-06-09 11:21:40 -0300

    test-with0my-email

--- pages/api/hyvor_talk/onCreateComment.ts
+++ pages/api/hyvor_talk/onCreateComment.ts
@@ -44,190 +44,192 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-    if(req.method?.toLowerCase()=='post'){
-      try{
-        // const bodyBuffer = await buffer(req);
-        //const givenSignature = (req.headers['X_SIGNATURE']??req.headers['x-signature'])?.toString()??'';
-        //if(givenSignature){
-        // const bodyJSON = bodyBuffer!.toString();
-        const body = req.body//JSON.parse(bodyJSON); 
-        const {
-            //event,
-            data
-        } = body;
+  return res.status(200).json({ data:'OK'});
+
+//     if(req.method?.toLowerCase()=='post'){
+//       try{
+//         // const bodyBuffer = await buffer(req);
+//         //const givenSignature = (req.headers['X_SIGNATURE']??req.headers['x-signature'])?.toString()??'';
+//         //if(givenSignature){
+//         // const bodyJSON = bodyBuffer!.toString();
+//         const body = req.body//JSON.parse(bodyJSON); 
+//         const {
+//             //event,
+//             data
+//         } = body;
          
-        let locale = req.cookies.NEXT_LOCALE || defaultLocale;
+//         let locale = req.cookies.NEXT_LOCALE || defaultLocale;
         
-        let url = data?.page?.url??'';
-        // const userId = data?.user?.sso_id??undefined;
-        const parent = data?.parent;
-        const name = data?.user?.name??undefined;
-        // // const body_html = data?.body_html??'';
-        const identifier = data?.page?.identifier??'';
-        const [elementType,elementId] = (identifier?.split('-')??[undefined,undefined]);
-        let cycle=null;
-        let post=null;
-        let work=null;
+//         let url = data?.page?.url??'';
+//         // const userId = data?.user?.sso_id??undefined;
+//         const parent = data?.parent;
+//         const name = data?.user?.name??undefined;
+//         // // const body_html = data?.body_html??'';
+//         const identifier = data?.page?.identifier??'';
+//         const [elementType,elementId] = (identifier?.split('-')??[undefined,undefined]);
+//         let cycle=null;
+//         let post=null;
+//         let work=null;
 
-        let elementTitle=data.page.title;
+//         let elementTitle=data.page.title;
 
-        let to:{email:string,name?:string}[] = [];
-        switch(elementType){
-          case 'cycle':
-            cycle = await prisma.cycle.findFirst({
-              where:{id:+elementId},
-              select:{
-                title:true,
-                languages:true,
-                creator:{select:{email:true,name:true}},
-                participants:{select:{email:true,name:true}},
-              }
-            });
-            elementTitle=cycle?.title;
-            const languages = cycle?.languages.split(",")
-            locale=languages?.length ? languages[0] : locale;
-          break;
-          case 'post':
-            post = await prisma?.post.findFirst({
-              where:{id:+elementId},
-              select:{
-                title:true,
-                language:true,
-                creator:{select:{name:true,email:true}}
-              }
-            });
-            elementTitle=post?.title;
-            locale=post?.language ?? locale;
-          break;  
-          case 'work':
-            work = await prisma.work.findFirst({
-              where:{id:+elementId},
-              select:{title:true,language:true}
-            });
-            elementTitle=work?.title;
-            locale=work?.language ?? locale;
-          break;  
-        }
-        if(!work && !post && !cycle)
-          return res.status(200).json({ data:{emailSend:false} });
+//         let to:{email:string,name?:string}[] = [];
+//         switch(elementType){
+//           case 'cycle':
+//             cycle = await prisma.cycle.findFirst({
+//               where:{id:+elementId},
+//               select:{
+//                 title:true,
+//                 languages:true,
+//                 creator:{select:{email:true,name:true}},
+//                 participants:{select:{email:true,name:true}},
+//               }
+//             });
+//             elementTitle=cycle?.title;
+//             const languages = cycle?.languages.split(",")
+//             locale=languages?.length ? languages[0] : locale;
+//           break;
+//           case 'post':
+//             post = await prisma?.post.findFirst({
+//               where:{id:+elementId},
+//               select:{
+//                 title:true,
+//                 language:true,
+//                 creator:{select:{name:true,email:true}}
+//               }
+//             });
+//             elementTitle=post?.title;
+//             locale=post?.language ?? locale;
+//           break;  
+//           case 'work':
+//             work = await prisma.work.findFirst({
+//               where:{id:+elementId},
+//               select:{title:true,language:true}
+//             });
+//             elementTitle=work?.title;
+//             locale=work?.language ?? locale;
+//           break;  
+//         }
+//         if(!work && !post && !cycle)
+//           return res.status(200).json({ data:{emailSend:false} });
 
-        if(parent && parent.user?.email!=data.user.email){
-            to=[{email:`${parent.user?.email}`,name:parent.user?.name}];
-            url=url?`${url}?ht-comment-id=${data.id}`:'';
-        }
-        else{
-          if(post && post.creator?.email!=data.user.email)
-              to=[{email:post.creator?.email!,name:post.creator.name!}]
-          else if(cycle){
-            const idx = cycle?.participants.findIndex(p=>p.email==data.user.email);
-            if(idx>=0)cycle?.participants.splice(idx!,1);
-            to=cycle.participants.map((p)=>({email:p.email!,name:p.name!}));
-            if(cycle.creator.email!=data.user.email)to.push({email:cycle.creator.email!,name:cycle.creator.name!});//to=[{email:cycle.creator.email!}]//
-          }
-          else if(work){
-            const url = `${WEBAPP_URL}/api/hyvor_talk/searchComments?id=${elementType}-${elementId}`;
-            const fr = await fetch(url);
-            const json = await fr.json();
-            json?.data?.data?.forEach((e:{user:{email:string,name:string}}) => {
-              if(e.user.email!=data.user.email)to.push({email:e.user.email,name:e.user.name!??e.user.email});
-            });
-            //to=json?.data?.data?.map((j:{user:{email:string}})=>({email:j.user.email}));
-          }
-        }
-        //let sense = (event??'').replace(/^\w+\.(\w+)/g,'$1');
-        //const msg=`hyvor-talk-comment-${sense}!|!{"userName":"${name}","cycleTitle":"${title}"}`;
-        if(!to?.length)return res.status(200).json({ data:{emailSend:false,notUsersToSend:true} });
+//         if(parent && parent.user?.email!=data.user.email){
+//             to=[{email:`${parent.user?.email}`,name:parent.user?.name}];
+//             url=url?`${url}?ht-comment-id=${data.id}`:'';
+//         }
+//         else{
+//           if(post && post.creator?.email!=data.user.email)
+//               to=[{email:post.creator?.email!,name:post.creator.name!}]
+//           else if(cycle){
+//             const idx = cycle?.participants.findIndex(p=>p.email==data.user.email);
+//             if(idx>=0)cycle?.participants.splice(idx!,1);
+//             to=cycle.participants.map((p)=>({email:p.email!,name:p.name!}));
+//             if(cycle.creator.email!=data.user.email)to.push({email:cycle.creator.email!,name:cycle.creator.name!});//to=[{email:cycle.creator.email!}]//
+//           }
+//           else if(work){
+//             const url = `${WEBAPP_URL}/api/hyvor_talk/searchComments?id=${elementType}-${elementId}`;
+//             const fr = await fetch(url);
+//             const json = await fr.json();
+//             json?.data?.data?.forEach((e:{user:{email:string,name:string}}) => {
+//               if(e.user.email!=data.user.email)to.push({email:e.user.email,name:e.user.name!??e.user.email});
+//             });
+//             //to=json?.data?.data?.map((j:{user:{email:string}})=>({email:j.user.email}));
+//           }
+//         }
+//         //let sense = (event??'').replace(/^\w+\.(\w+)/g,'$1');
+//         //const msg=`hyvor-talk-comment-${sense}!|!{"userName":"${name}","cycleTitle":"${title}"}`;
+//         if(!to?.length)return res.status(200).json({ data:{emailSend:false,notUsersToSend:true} });
 
-        locale=LOCALES[locale]??defaultLocale;
-        const path = join(process.cwd(),'locales',locale,'onCommentCreated.json');
+//         locale=LOCALES[locale]??defaultLocale;
+//         const path = join(process.cwd(),'locales',locale,'onCommentCreated.json');
 
-        const rf = promisify(readFile);
-        const jsonStr = await rf(path);
-        const json = JSON.parse(jsonStr.toString());
-        const dict = (k:string,specs?:Record<string,any>)=>{
-          let val = json[k];
-          if(specs){
-            const specsEntries = Object.entries(specs);
-            if(specsEntries.length){
-              let i = 0;
-              for(;i<specsEntries.length;i++){
-                const re=new RegExp(`{{${specsEntries[i][0]}}}`);
-                const vr = val.replace(re,specsEntries[i][1]);
-                val = vr ?? val;
-              }
-            }
-          }
-          return val;
-        }
+//         const rf = promisify(readFile);
+//         const jsonStr = await rf(path);
+//         const json = JSON.parse(jsonStr.toString());
+//         const dict = (k:string,specs?:Record<string,any>)=>{
+//           let val = json[k];
+//           if(specs){
+//             const specsEntries = Object.entries(specs);
+//             if(specsEntries.length){
+//               let i = 0;
+//               for(;i<specsEntries.length;i++){
+//                 const re=new RegExp(`{{${specsEntries[i][0]}}}`);
+//                 const vr = val.replace(re,specsEntries[i][1]);
+//                 val = vr ?? val;
+//               }
+//             }
+//           }
+//           return val;
+//         }
         
-        const postFix=cycle?'-sumary':'';
-        const subject=dict(`subject-${elementType}${postFix}`,{title:elementTitle});
-        const title=!cycle 
-          ? dict(`title-${elementType}`,{title:elementTitle,name})
-          : dict(`title-${elementType}${postFix}`,{title:elementTitle,first3UsersNames:to.slice(0,3).map(t=>t.name??t.email).join(', ')});
-        const about=dict(`about-${elementType}${postFix}`);
-        const aboutEnd=dict(`aboutEnd`);
-        const urllabel=dict('urlLabel');
-        const unsubscribe="";//dict('unsubscribe');
-
-        if(cycle){
-          const comentEmailSaved = await prisma.comentCreatedDaily.create({
-            data:{
-              //TODO test only
-              to:[{email:'gbanoaol@gmail.com'}].map(t=>t.email).join(','),
-              //TODO test only
-              // to:to.map(t=>t.email).join(','),
-              subject,
-              etitle:title,
-              about,
-              aboutEnd,
-              eurl:url,
-              urllabel,
-              unsubscribe
-            }
-          });
-          if(!(global as any).sendEmailWithComentCreatedSumaryCronJob){
-            // const cronTime = '0 0 20 * * *';
-            //TODO test only
-            const cronTime = '10 * * * * *';
-            //TODO test only
-            const dt = sendAt(cronTime);
-            console.log(`The job would run at: ${dt.toISO()}`);
-            (global as any).sendEmailWithComentCreatedSumaryCronJob = new CronJob(
-              cronTime, // cronTime
-              async function () {
-                await sendEmailWithComentCreatedSumary();
-              }, // onTick
-              null, // onComplete
-              true, // start
-              'America/Sao_Paulo' // timeZone
-            );
-          }
-          return res.status(200).json({ data:{comentEmailSaved} });    
-        }
+//         const postFix=cycle?'-sumary':'';
+//         const subject=dict(`subject-${elementType}${postFix}`,{title:elementTitle});
+//         const title=!cycle 
+//           ? dict(`title-${elementType}`,{title:elementTitle,name})
+//           : dict(`title-${elementType}${postFix}`,{title:elementTitle,first3UsersNames:to.slice(0,3).map(t=>t.name??t.email).join(', ')});
+//         const about=dict(`about-${elementType}${postFix}`);
+//         const aboutEnd=dict(`aboutEnd`);
+//         const urllabel=dict('urlLabel');
+//         const unsubscribe="";//dict('unsubscribe');
+// debugger;
+//         if(cycle){
+//           const comentEmailSaved = await prisma.comentCreatedDaily.create({
+//             data:{
+//               //TODO test only
+//               to:[{email:'gbanoaol@gmail.com'}].map(t=>t.email).join(','),
+//               //TODO test only
+//               // to:to.map(t=>t.email).join(','),
+//               subject,
+//               etitle:title,
+//               about,
+//               aboutEnd,
+//               eurl:url,
+//               urllabel,
+//               unsubscribe
+//             }
+//           });
+//           if(!(global as any).sendEmailWithComentCreatedSumaryCronJob){
+//             // const cronTime = '0 0 20 * * *';
+//             //TODO test only
+//             const cronTime = '10 * * * * *';
+//             //TODO test only
+//             const dt = sendAt(cronTime);
+//             console.log(`The job would run at: ${dt.toISO()}`);
+//             (global as any).sendEmailWithComentCreatedSumaryCronJob = new CronJob(
+//               cronTime, // cronTime
+//               async function () {
+//                 await sendEmailWithComentCreatedSumary();
+//               }, // onTick
+//               null, // onComplete
+//               true, // start
+//               'America/Sao_Paulo' // timeZone
+//             );
+//           }
+//           return res.status(200).json({ data:{comentEmailSaved} });    
+//         }
         
-        const emailSend = await sendEmailOnCommentCreated({
-            //TODO test only
-            to:[{email:'gbanoaol@gmail.com'}],
-            // remove mujeresinsumisasupn@gmail.com from https://mandrillapp.com/settings/rejections/?subaccount= rejection deny list
-            //TODO test only
-          // to,
-          subject,
-          specs:{
-            etitle:title,
-            about,
-            aboutEnd,
-            eurl:url,
-            urllabel,
-            unsubscribe
-          },
-        });
+//         const emailSend = await sendEmailOnCommentCreated({
+//             //TODO test only
+//             to:[{email:'gbanoaol@gmail.com'}],
+//             // remove mujeresinsumisasupn@gmail.com from https://mandrillapp.com/settings/rejections/?subaccount= rejection deny list
+//             //TODO test only
+//           // to,
+//           subject,
+//           specs:{
+//             etitle:title,
+//             about,
+//             aboutEnd,
+//             eurl:url,
+//             urllabel,
+//             unsubscribe
+//           },
+//         });
         
-        return res.status(200).json({ data:{emailSend} });
-      }
-      catch(e){
-          console.error(e);
-          return res.status(400).json({ error:e?.toString() });
-      }
-    }
+//         return res.status(200).json({ data:{emailSend} });
+//       }
+//       catch(e){
+//           console.error(e);
+//           return res.status(400).json({ error:e?.toString() });
+//       }
+//     }
 }
