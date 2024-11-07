09535c4c5b52c002396927f0695f81ce67b058a1
Henry200379 <henry@eureka.club>
2024-11-07 11:17:41 -0500

    paginas pasarela de pago a material

--- pages/payment_success.tsx
+++ pages/payment_success.tsx
@@ -3,13 +3,14 @@ import { useState, useEffect } from 'react';
 import { getSession } from 'next-auth/react';
 import useTranslation from 'next-translate/useTranslation';
 import Footer from '@/components/layouts/Footer';
-import Button from 'react-bootstrap/Button';
+//import Button from 'react-bootstrap/Button';
 import { Box } from '@mui/material';
 import Link from 'next/link';
-import {Col,Row} from 'react-bootstrap';
+//import {Col,Row} from 'react-bootstrap';
 import SimpleLayout from '@/src/components/layouts/SimpleLayout';
 import { useRouter } from 'next/router';
 import { Session } from '@/src/types';
+import {Button,Grid} from '@mui/material'
 
 interface Props {
   session: Session
@@ -35,10 +36,9 @@ const StripePaymentSuccessPage: NextPage<Props> = ({ session }) => {
   return (
     <SimpleLayout title="Payment Success" showNavBar={false} showFooter={false}>
       <>
-        <Box >
-          <Row className="d-flex justify-content-between">
-            <Col className='col-12'>
-              <Row className='p-4'>
+        <Box padding={3}>
+         
+              <Grid container >
                 <Link href="/" replace >
                   <a className="d-flex align-items-center">
                     <aside className="d-flex justify-content-around align-items-center">
@@ -51,29 +51,28 @@ const StripePaymentSuccessPage: NextPage<Props> = ({ session }) => {
                     </aside>
                   </a>
                 </Link>
-              </Row>
-              <Box className='d-flex flex-column flex-xl-row'
+              </Grid>
+              <Box padding={3}
                 sx={{
                   backgroundImage: { sm:"url('/registro_desktop_about_bg.webp')"},
                   backgroundRepeat: "no-repeat",
                   backgroundSize: {sm:`100% auto`},
-                  height: { sm: '500px', md: '750px' },//lg:'500px'
+                  height: { sm: '500px', md: '450px' },//lg:'500px'
                 }}
               >
-                <Col className=' d-flex w-100 justify-content-center mt-5'>
-                  <Box className=' d-flex flex-column'>
-                    <Row className='p-3 '><h1 className='text-primary text-center  mb-5'><b>{t('successText')}</b></h1></Row>
-                    <Row className='p-3 '><h1 className='text-primary text-center   mb-5'><b>{t('successExtraText')}</b></h1></Row>
-                    <Row className='w-100  p-2'>
-                      <Button className={`mt-4 btn btn-eureka  w-100`} onClick={() => router.push(`/cycle/${cycleId}`)}>
-                        {t('successButtomText')}
+                <Grid container justifyContent="center" alignItems="center" alignContent={'center'}>
+                  <Box padding={3}>
+                    <Grid container justifyContent="center" alignItems="center" alignContent={'center'}><h1 className='text-primary text-center  mb-5'><b>{t('successText')}</b></h1></Grid>
+                    <Grid container justifyContent="center" alignItems="center" alignContent={'center'} ><h1 className='text-primary text-center   mb-5'><b>{t('successExtraText')}</b></h1></Grid>
+                    <Grid container justifyContent="center" alignItems="center" alignContent={'center'}>
+                      <Button variant="contained" size="large"  onClick={() => router.push(`/cycle/${cycleId}`)}>
+                        <b>{t('successButtomText')}</b>
                       </Button>
-                    </Row>
+                    </Grid>
                   </Box>
-                </Col>
+                </Grid>
               </Box>
-            </Col>
-            </Row>
+            
          
         </Box>
         <Footer />
