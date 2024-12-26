89131decada2c2a664c8b18a2c2e3e6f18538e54
Geordanis B. Vega <gbanoaol@gmail.com>
2024-12-26 09:23:36 -0300

    fe-countdown-seconds-reactive

--- pages/participar/components/Countdown.tsx
+++ pages/participar/components/Countdown.tsx
@@ -1,5 +1,5 @@
 import { Stack, Typography } from "@mui/material";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 interface Props {
     startDate:Date;
@@ -8,8 +8,20 @@ const Countdown:FC<Props> = ({startDate}) => {
     const days = Math.floor((startDate.getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24));
     const hours = Math.floor((startDate.getTime() - new Date().getTime()) / (1000 * 60 * 60) % 24);
     const minutes = Math.floor((startDate.getTime() - new Date().getTime()) / (1000 * 60) % 60);
-    const seconds = Math.floor((startDate.getTime() - new Date().getTime()) / 1000 % 60);
+    const [seconds,setseconds] = useState(Math.floor((startDate.getTime() - new Date().getTime()) / 1000 % 60));
     
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setseconds((prev) => {
+                if(prev === 0){
+                    return 59;
+                }
+                return prev - 1;
+            });
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
     return <Stack direction="row" spacing={1}>
         <Stack direction={'column'} padding={1} alignItems={'center'} justifyContent={'center'} style={{backgroundColor:'var(--color-danger)',width:'6rem', height:'6rem'}}>
             <Typography color="white" fontWeight={'bolder'} fontSize={'1.5rem'}>{days}</Typography>
