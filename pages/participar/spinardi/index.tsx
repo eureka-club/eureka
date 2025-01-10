34803263fe9d6e4d2e457bf99fdb9cd6ae3728e6
Geordanis B. Vega <gbanoaol@gmail.com>
2025-01-10 10:20:56 -0300

    hf-added-google-tag-manager-for-spinardi-page

--- pages/participar/spinardi/index.tsx
+++ pages/participar/spinardi/index.tsx
@@ -29,22 +29,23 @@ const Spinardi: NextPage<Props> = ({session,cycleId}) => {
   
   return (
     <>
+      <Script
+        id="gtag-base"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', 'GTM-PNQQBFG3');
+          `,
+        }}
+      />
       <Head>
         <meta name="title" content={t('title page')}></meta>
         <meta name="description" content={t('title page')}></meta>
-        <Script
-                    id="gtag-base"
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                      __html: `
-                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer', 'GTM-PNQQBFG3');
-                      `,
-                    }}
-                  />
+        
       </Head>
       <style jsx global>{`
         body {
