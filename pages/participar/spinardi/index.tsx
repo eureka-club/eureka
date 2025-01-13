e1f784eacacc5ae1737a8ea70c61dd87681b0754
Geordanis B. Vega <gbanoaol@gmail.com>
2025-01-13 13:26:15 -0300

    fe-removing-gtm-from-spinardi-page-now-will-be-the-global-gtm

--- pages/participar/spinardi/index.tsx
+++ pages/participar/spinardi/index.tsx
@@ -29,7 +29,7 @@ const Spinardi: NextPage<Props> = ({session,cycleId}) => {
   
   return (
     <>
-      <Script
+      {/* <Script
                   id="gtag-base"
                   strategy="afterInteractive"
                   dangerouslySetInnerHTML={{
@@ -41,7 +41,7 @@ const Spinardi: NextPage<Props> = ({session,cycleId}) => {
                       })(window,document,'script','dataLayer', 'GTM-PNQQBFG3');
                     `,
                   }}
-                />
+                /> */}
       <Head>
         <meta name="title" content={t('title page')}></meta>
         <meta name="description" content={t('title page')}></meta>
@@ -182,14 +182,14 @@ const Spinardi: NextPage<Props> = ({session,cycleId}) => {
             </Box>
           </Box>
         </Stack>
-        <noscript>
+        {/* <noscript>
             <iframe
               src={`https://www.googletagmanager.com/ns.html?id=GTM-PNQQBFG3`}
               height="0"
               width="0"
               style={{ display: 'none', visibility: 'hidden' }}
             />
-        </noscript>
+        </noscript> */}
     </>
   );
 };
